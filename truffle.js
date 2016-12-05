93790506c6e1e543a48d30b9379acf49bf00a76d
Mike Fluff <mikefluff@Mikes-MacBook-Air-2.local>
2016-12-05 14:45:29 +0300

    Initial webpack setup

--- truffle.js
+++ truffle.js
@@ -1,16 +1,7 @@
 module.exports = {
-  // build: {
-  //   "index.html": "index.html",
-  //   "app.js": [
-  //     "javascripts/app.js"
-  //   ],
-  //   "app.css": [
-  //     "stylesheets/app.css"
-  //   ],
-  //   "images/": "images/"
-  // },
   rpc: {
-    host: "localhost",
+    host: 'localhost',
     port: 8545
-  }
-};
+  },
+  migrations_directory: './migrations'
+}
