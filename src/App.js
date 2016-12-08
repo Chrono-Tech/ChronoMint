2cd8f15b04a429d4e93cda7bd776be8542e29918
Mike Fluff <mikefluff@Mikes-MacBook-Air-2.local>
2016-12-08 17:54:36 +0300

    Material-ui for redux-form added, initial component setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
+import { reducer as reduxFormReducer } from 'redux-form'
 
 import './App.css'
 
@@ -11,12 +14,20 @@ import LOCContainer from 'components/LOC/LOCContainer'
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
+const reducer = combineReducers({
+  form: reduxFormReducer // mounted under "form"
+})
+const store =
+  (window.devToolsExtension ? window.devToolsExtension()(createStore) : createStore)(reducer)
+
 class App extends Component {
   render () {
     return (
+ <Provider store={store}>
       <div className="App">
         <LOCContainer web3={this.props.web3} />
       </div>
+ </Provider>
     )
   }
 }
--- src/components/LOC/LOCContainer.js
+++ src/components/LOC/LOCContainer.js
@@ -6,6 +6,16 @@ import {deepOrange500} from 'material-ui/styles/colors';
 import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { reduxForm, Field } from 'redux-form'
+import MenuItem from 'material-ui/MenuItem'
+import { RadioButton } from 'material-ui/RadioButton'
+import {
+  Checkbox,
+  RadioButtonGroup,
+  SelectField,
+  TextField,
+  Toggle
+} from 'redux-form-material-ui'
 
 import ChronoMint from 'contracts/ChronoMint.sol';
 import Web3 from 'web3';
@@ -22,8 +32,6 @@ const muiTheme = getMuiTheme({
     accent1Color: deepOrange500,
   },
 });
-
-
 
 const provider = new Web3.providers.HttpProvider('http://localhost:8545')
 ChronoMint.setProvider(provider);
@@ -86,6 +94,25 @@ render() {
           </Dialog>
           <h1>ChronoMint</h1>
           <h2>Chronobank Manager</h2>
+      <form>
+        <Field name="username" component={TextField} hintText="Street"/>
+
+        <Field name="plan" component={SelectField} hintText="Select a plan">
+          <MenuItem value="monthly" primaryText="Monthly"/>
+          <MenuItem value="yearly" primaryText="Yearly"/>
+          <MenuItem value="lifetime" primaryText="Lifetime"/>
+        </Field>
+
+        <Field name="agreeToTerms" component={Checkbox} label="Agree to terms?"/>
+
+        <Field name="receiveEmails" component={Toggle} label="Please spam me!"/>
+
+        <Field name="bestFramework" component={RadioButtonGroup}>
+          <RadioButton value="react" label="React"/>
+          <RadioButton value="angular" label="Angular"/>
+          <RadioButton value="ember" label="Ember"/>
+        </Field>
+      </form>
           <RaisedButton
             label="My First Action"
             secondary={true}
@@ -98,4 +125,8 @@ render() {
 
 }
 
+// Decorate with redux-form
+LOCContainer = reduxForm({
+  form: 'LOCContainer'
+})(LOCContainer)
 export default LOCContainer
