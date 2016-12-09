493f97c7d1778d3d95b7d5c8eacf47fdae44a292
Greg Mikeska <gmikeska07@gmail.com>
2016-12-09 14:51:21 -0600

    LOC and LH contracts created

--- contracts/ChronoMint.sol
+++ contracts/ChronoMint.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.4;
 
-/*import "Debugging.sol";*/
+
 import "Managed.sol";
 import "Stub.sol";
 
@@ -14,36 +14,33 @@ contract ChronoMint is Managed {
   uint public liquidityPercentage;
   uint public insurancePercentage;
   uint public insuranceDuration;
+  mapping(string => uint) private totals;
   mapping(uint => LHContract) public lhContracts;
   mapping(uint => LOC) public offeringCompanies;
 
-  struct LOC {
-        uint index;
-        string name;
-        string website;
-        Status status;
-        address controller;
-        uint issueLimit;
-        uint redeemed;
-        string publishedHash;
-    }
-
-  struct LHContract {
-        string currency;
-        uint rate;
-        uint[] offeringCompanies;
-    }
 
 
-  function addLOC(string _name, string _website, Status _status, address _controller, uint _issueLimit, uint _redeemed, string _publishedHash) onlyAuthorized {
-    offeringCompanies[LOCCount] = LOC(LOCCount, _name, _website, _status, _controller, _issueLimit, _redeemed, _publishedHash);
-    LOCCount++;
+  function addLOC(string _name, string _website, address _controller, uint _issueLimit, uint _redeemed, string _publishedHash) onlyAuthorized returns(uint){
+    uint id = totals['loc'];
+    LOC newContract = new LOC(totals['loc'], _name, _website, _controller, _issueLimit, _redeemed, _publishedHash);
+    offeringCompanies[id] = newContract;
+    totals['loc']++;
+    return id;
+  }
+
+  function addLHContract(string _currency, uint _rate) onlyAuthorized returns(uint) {
+    uint id = totals['lh'];
+    LHContract newContract = new LHContract(id, _currency, _rate);
+    lhContracts[id] = newContract;
+    totals['lh']++;
+    return id;
   }
 
   function setTimeContract(address _tc) onlyAuthorized returns(address) {
     timeContract = _tc;
     return timeContract;
   }
+
 
   function setRewardsContract(address _rc) onlyAuthorized returns(bool) {
     rewardsContract = _rc;
@@ -59,6 +56,55 @@ contract ChronoMint is Managed {
   {
     throw;
   }
+}
+
+contract LHContract is Managed {
+  uint id;
+  uint rate;
+  string currency;
+  address[] controller;
+  LOC[] offeringCompanies;
+  address chronoMint;
+
+  function LHContract(uint _id, string _currency, uint _rate){
+    chronoMint = msg.sender;
+    id = _id;
+    currency = _currency;
+    rate = _rate;
+
+  }
+
+  function addLOC(LOC loc) onlyAuthorized returns (bool){
+    offeringCompanies.push(loc);
+  }
+
+}
+contract LOC is Managed{
+  enum Status  {active, suspended, bankrupt}
+  uint id;
+  string name;
+  string website;
+  Status status;
+  address controller;
+  uint issueLimit;
+  uint redeemed;
+  string publishedHash;
+  address chronoMint;
+
+  function LOC(uint _id, string _name, string _website, address _controller, uint _issueLimit, uint _redeemed, string _publishedHash){
+    chronoMint = msg.sender;
+    id = _id;
+
+    name = _name;
+    website = website;
+    status = Status.active;
+    controller = _controller;
+    issueLimit = _issueLimit;
+    redeemed = _redeemed;
+    publishedHash = _publishedHash;
+
+  }
+
 }
 
 contract TimeContract is Stub {}
