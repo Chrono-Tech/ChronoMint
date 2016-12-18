5d58afac82941115c51ef0bb880549d04ed26473
Greg Mikeska <gmikeska07@gmail.com>
2016-12-18 06:14:10 -0600

    push a file I missed

---
+++ contracts/ChronoMintDeployable.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.4;
+
+import "Configurable.sol";
+
+contract ChronoMintDeployable is Configurable {
+  address chronoMint;
+  mapping(string => string) stringSettings;
+
+  modifier onlyMint() {
+    if (isMint(msg.sender)) {
+      _;
+      } else {
+        return;
+      }
+  }
+  function ChronoMintDeployable() {
+    chronoMint = msg.sender;
+  }
+  function isMint(address _ad) returns(bool) {
+    if (_ad == address(chronoMint))
+      return true;
+    else
+      return false;
+  }
+  function setValue(string name, uint value) onlyMint() {
+      setVal(name, value);
+  }
+
+  function approved() onlyMint();
+}
