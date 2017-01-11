24da77ef0edc3f696b1b1427ed419ceb52bcedc1
Mike Fluff <mikefluff@mikefluff.com>
2017-01-11 13:28:40 +0200

    Pending operations list has been added

---
+++ src/containers/OpList.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import {Link} from 'react-router';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentCreate from 'material-ui/svg-icons/content/create';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import {pink500, grey200, grey500} from 'material-ui/styles/colors';
+import PageBase from '../components/PageBase';
+import RaisedButton from 'material-ui/RaisedButton';
+import Data from '../data';
+
+const TablePage = () => {
+
+  const styles = {
+    floatingActionButton: {
+      margin: 0,
+      top: 'auto',
+      right: 20,
+      bottom: 20,
+      left: 'auto',
+      position: 'fixed',
+    },
+    editButton: {
+      fill: grey500
+    },
+    columns: {
+      id: {
+        width: '5%'
+      },
+      name: {
+        width: '40%'
+      },
+      price: {
+        width: '20%'
+      },
+      category: {
+        width: '20%'
+      },
+      edit: {
+        width: '15%'
+      }
+    }
+  };
+
+  return (
+    <PageBase title="Operations List"
+              navigation="ChronoMint / Operations List">
+
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderColumn style={styles.columns.id}>ID</TableHeaderColumn>
+            <TableHeaderColumn style={styles.columns.name}>Operation</TableHeaderColumn>
+            <TableHeaderColumn style={styles.columns.price}>Initiator</TableHeaderColumn>
+            <TableHeaderColumn style={styles.columns.category}>Category</TableHeaderColumn>
+            <TableHeaderColumn style={styles.columns.edit}>Action</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Data.tablePage.items.map(item =>
+            <TableRow key={item.id}>
+              <TableRowColumn style={styles.columns.id}>{item.id}</TableRowColumn>
+              <TableRowColumn style={styles.columns.name}>{item.name}</TableRowColumn>
+              <TableRowColumn style={styles.columns.price}>{item.price}</TableRowColumn>
+              <TableRowColumn style={styles.columns.category}>{item.category}</TableRowColumn>
+              <TableRowColumn style={styles.columns.edit}>
+                <Link className="button" to="/form">
+                  <RaisedButton label="Approve"
+                                style={styles.saveButton}
+                                type="submit"
+                                primary={true}/>
+                </Link>
+              </TableRowColumn>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </PageBase>
+  );
+};
+
+export default TablePage;
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,18 +5,22 @@ import NotFoundPage from '../containers/NotFoundPage.js';
 import LoginPage from '../containers/LoginPage';
 import FormPage from '../containers/FormPage';
 import TablePage from '../containers/TablePage';
+import OpList from '../containers/OpList';
 import Dashboard from '../containers/DashboardPage';
 import Dashboard2 from '../containers/Dashboard2Page';
 
 export default (store) => (
     <Route>
+        <IndexRoute component={Dashboard}/>
         <Route path="login" component={LoginPage}/>
         <Route path="/" component={App}>
-            <IndexRoute component={Dashboard}/>
+
             <Route path="dashboard" component={Dashboard}/>
             <Route path="dashboard2" component={Dashboard2}/>
             <Route path="form" component={FormPage}/>
+            <Route path="form2" component={FormPage}/>
             <Route path="table" component={TablePage}/>
+            <Route path="operation" component={OpList}/>
             <Route path="*" component={NotFoundPage}/>
         </Route>
     </Route>
