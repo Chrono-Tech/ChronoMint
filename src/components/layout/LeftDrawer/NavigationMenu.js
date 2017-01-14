ae96dc0fef453383c7243b0cf423b29a92f29b2b
Dmitry Semenovsky <DS@Dmitrys-MacBook-Pro.local>
2017-01-14 11:10:36 -0500

    Separate navigation menu

--- src/components/layout/LeftDrawer/NavigationMenu.js
+++ src/components/layout/LeftDrawer/NavigationMenu.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Assessment from 'material-ui/svg-icons/action/assessment';
+import GridOn from 'material-ui/svg-icons/image/grid-on';
 import {white, darkWhite} from 'material-ui/styles/colors';
 import MenuItem from 'material-ui/MenuItem';
 import {Link} from 'react-router';
@@ -32,14 +34,14 @@ class NavigationMenu extends Component {
                     key="locs"
                     style={styles.menuItem}
                     primaryText="LOCs"
-                    leftIcon={<Assessment color={darkWhite}/>}
+                    leftIcon={<GridOn color={darkWhite}/>}
                     containerElement={<Link to={{pathname: '/table'}} />}
                 />
                 <MenuItem
                     key="operations"
                     style={styles.menuItem}
                     primaryText="Operations"
-                    leftIcon={<Assessment color={darkWhite}/>}
+                    leftIcon={<GridOn color={darkWhite}/>}
                     containerElement={<Link to={{pathname: '/operations'}} />}
                 />
             </div>
--- src/data.js
+++ src/data.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import Assessment from 'material-ui/svg-icons/action/assessment';
-import GridOn from 'material-ui/svg-icons/image/grid-on';
-import PermIdentity from 'material-ui/svg-icons/action/perm-identity';
-import Web from 'material-ui/svg-icons/av/web';
-import {cyan600, pink600, purple600, darkWhite} from 'material-ui/styles/colors';
 import ExpandLess from 'material-ui/svg-icons/navigation/expand-less';
 import ExpandMore from 'material-ui/svg-icons/navigation/expand-more';
 import ChevronRight from 'material-ui/svg-icons/navigation/chevron-right';
