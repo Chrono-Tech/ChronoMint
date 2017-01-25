dd5e8b9e43418b56f41d768c6ce839e32b6f2161
Dmitry Semenovsky <dsemenovsky@gmail.com>
2017-01-24 22:10:10 -0500

    Restyle drawer

--- src/components/layout/LeftDrawer/index.js
+++ src/components/layout/LeftDrawer/index.js
@@ -2,17 +2,20 @@ import React,  { PropTypes } from 'react';
 import Drawer from 'material-ui/Drawer';
 import UserInfo from './UserInfo';
 import NavigationMenu from './NavigationMenu';
+import './style.scss';
 
 const styles = {
     container: {
-        paddingTop: 56
+        paddingTop: 56,
+        backgroundColor: '#fff'
     }
 };
 
-const LeftDrawer = ({navDrawerOpen, menus}) => {
+const LeftDrawer = ({navDrawerOpen}) => {
     return (
         <Drawer
             docked={true}
+            className="left-drawer"
             containerStyle={styles.container}
             open={navDrawerOpen}>
             <UserInfo />
