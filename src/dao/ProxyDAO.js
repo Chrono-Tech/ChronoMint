2e00f4f52f385ada3799835dc44e80f0134df7fb
Boris Shevchenko <info@bshevchenko.ru>
2017-02-21 20:35:47 +0300

    ProxyDAO optimized error handling

--- src/dao/ProxyDAO.js
+++ src/dao/ProxyDAO.js
@@ -1,5 +1,6 @@
 import AbstractProxyDAO from './AbstractProxyDAO';
 import contract from 'truffle-contract';
+import isEthAddress from '../utils/isEthAddress';
 
 const json = require('../contracts/ChronoBankAssetProxy.json');
 const ChronoBankAssetProxy = contract(json);
@@ -10,14 +11,24 @@ export default class ProxyDAO extends AbstractProxyDAO {
         ChronoBankAssetProxy.setProvider(this.web3.currentProvider);
 
         this.contractDeployed = null;
-        ChronoBankAssetProxy.at(address).then(deployed => this.contractDeployed = deployed);
+        this.deployError = isEthAddress(address) ? null : 'invalid address passed';
 
-        this.contract = new Promise(resolve => {
+        if (this.deployError == null) {
+            ChronoBankAssetProxy.at(address)
+                .then(deployed => this.contractDeployed = deployed)
+                .catch(e => this.deployError = e);
+        }
+
+        this.contract = new Promise((resolve, reject) => {
             let interval = null;
             let callback = () => {
                 if (this.contractDeployed) {
                     clearInterval(interval);
                     resolve(this.contractDeployed);
+                }
+
+                if (this.deployError) {
+                    reject(this.deployError);
                 }
             };
             callback();
