ddcb9816df7132ed7903c3cc6b34e69abea3afce
Boris Shevchenko <info@bshevchenko.ru>
2017-02-27 14:22:24 +0300

    jasmine timeout interval (that ruined tests) increased

--- specs/init.js
+++ specs/init.js
@@ -1,3 +1,6 @@
 import initLocalStorageMock from './mock/localStorage';
 
+// we need enough time to test contract watch functionality
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
+
 initLocalStorageMock();
