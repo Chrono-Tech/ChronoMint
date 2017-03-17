ddae60ccffe73127aa766f942106c066e9483334
Pavel <pavel.dcv@gmail.com>
2017-03-17 10:16:31 +0700

    new timeout test

--- specs/init.js
+++ specs/init.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import initLocalStorageMock from './mock/localStorage';
 
 // we need enough time to test contract watch functionality
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 200000;
 
 initLocalStorageMock();
 
