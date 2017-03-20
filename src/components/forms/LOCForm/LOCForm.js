c90c8f74083a651e826fc7d923f3d69034bd72e9
gagarin55 <gagarin55@protonmail.com>
2017-03-20 12:44:21 +0300

    invalid import fixed

--- src/components/forms/LOCForm/LOCForm.js
+++ src/components/forms/LOCForm/LOCForm.js
@@ -1,22 +1,10 @@
 import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form/immutable';
 import {connect} from 'react-redux';
-import {DatePicker} from 'redux-form-material-ui'
+import {DatePicker, TextField} from 'redux-form-material-ui'
 import FileSelect from '../../common/IPFSFileSelect';
-import {TextField} from 'material-ui';
 import validate from './validate';
 import globalStyles from '../../../styles';
-import LocModel from '../../../models/LocModel'
-
-const renderTextField = ({ input, label, hint, meta: { touched, error }, ...custom }) => (
-    <TextField hintText={hint}
-               floatingLabelText={label}
-               fullWidth={false}
-               errorText={touched && error}
-               {...input}
-               {...custom}
-    />
-);
 
 const mapStateToProps = state => {
     const loc = state.get('loc').toJS();
@@ -33,12 +21,7 @@ const mapDispatchToProps = null;
 const mergeProps = null;
 const options = {withRef: true};
 
-@connect(mapStateToProps, mapDispatchToProps, mergeProps, options)
-@reduxForm({
-    form: 'LOCForm',
-    validate,
-})
-class LOCForm extends Component {
+export class LOCForm extends Component {
 
     render() {
         const {
@@ -48,14 +31,14 @@ class LOCForm extends Component {
         return (
             <form onSubmit={handleSubmit} name="LOCFormName">
 
-                <Field component={renderTextField}
+                <Field component={TextField}
                        style={globalStyles.form.firstField}
                        name="locName"
                        floatingLabelText="LOC title"
                 />
                 <br />
 
-                <Field component={renderTextField}
+                <Field component={TextField}
                        style={globalStyles.form.textField}
                        name="website"
                        hintText="http://..."
@@ -75,14 +58,14 @@ class LOCForm extends Component {
                 />
 
                 <h3 style={{marginTop: 20}}>Issuance parameters</h3>
-                <Field component={renderTextField}
+                <Field component={TextField}
                        style={globalStyles.form.firstField}
                        name="issueLimit"
                        type="number"
                        floatingLabelText="Allowed to be issued"
                 />
                 <br />
-                <Field component={renderTextField}
+                <Field component={TextField}
                        name="fee"
                        floatingLabelText="Insurance fee"
                        hintText={"0.0%"}
@@ -90,11 +73,14 @@ class LOCForm extends Component {
                        style={{marginTop: -8, pointerEvents: 'none'}}
                 />
 
-                <Field component={renderTextField} name="address" style={{display: 'none'}}/>
+                <Field component={TextField} name="address" style={{display: 'none'}}/>
 
             </form>
         );
     }
 }
 
-export default LOCForm;
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps, options)(reduxForm({
+    form: 'LOCForm',
+    validate,
+})(LOCForm))
