8317fa00492e9018ebeacbdcdf9065b4222af2e5
Pavel <pavel.dcv@gmail.com>
2017-04-10 12:35:40 +0700

    MINT-137 Implement blockchain reverter to specs

--- specs/init.js
+++ specs/init.js
@@ -4,6 +4,7 @@ import Web3 from 'web3'
 import initLocalStorageMock from './mock/localStorage'
 import OrbitDAO from '../src/dao/OrbitDAO'
 import AbstractContractDAO from '../src/dao/AbstractContractDAO'
+import Reverter from '../test/helpers/reverter'
 
 // we need enough time to test contract watch functionality
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000
@@ -11,13 +12,19 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000
 initLocalStorageMock()
 
 AbstractContractDAO._web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+const reverter = new Reverter(AbstractContractDAO._web3)
 
 const mockStore = configureMockStore([thunk])
 export let store = null
 
-beforeAll(() => {
+beforeAll((done) => {
   window.resolveWeb3 = Promise.resolve(null)
-  return OrbitDAO.init(null)
+  OrbitDAO.init(null)
+  reverter.snapshot(done)
+})
+
+afterAll((done) => {
+  reverter.revert(done)
 })
 
 beforeEach(() => {
--- specs/redux/wallet/wallet.spec.js
+++ specs/redux/wallet/wallet.spec.js
@@ -5,6 +5,7 @@ import {store} from '../../init'
 const account = UserDAO.web3.eth.accounts[0]
 
 describe('Time actions', () => {
+
   it('should show 0 time balance', () => {
     return store.dispatch(actions.updateTimeBalance(account)).then(() => {
       expect(store.getActions()).toContainEqual({'payload': 0, 'type': 'wallet/SET_TIME_BALANCE_SUCCESS'})
@@ -17,7 +18,7 @@ describe('Time actions', () => {
     })
   })
 
-  it.skip('should request time & show 1000 time balance #1', () => {
+  it('should request time & show 1000 time balance #1', () => {
     return store.dispatch(actions.requireTime(account)).then(() => {
       expect(store.getActions()).toContainEqual({
         payload: {
@@ -30,7 +31,7 @@ describe('Time actions', () => {
     })
   })
 
-  it.skip('should show error on second request time', () => {
+  it('should show error on second request time', () => {
     return store.dispatch(actions.requireTime(account)).then(() => {
       expect(store.getActions()).toContainEqual(
         {
@@ -43,51 +44,51 @@ describe('Time actions', () => {
     })
   })
 
-  it.skip('should show 1000 time balance #1', () => {
+  it('should show 1000 time balance #1', () => {
     return store.dispatch(actions.updateTimeBalance(account)).then(() => {
       expect(store.getActions()).toContainEqual({'payload': 1000, 'type': 'wallet/SET_TIME_BALANCE_SUCCESS'})
     })
   })
 
-  it.skip('should deposit 100 time', () => {
+  it('should deposit 100 time', () => {
     return store.dispatch(actions.depositTime(100, account)).then(() => {
-      expect(store.getActions()).toContainEqual({'type': 'wallet/SET_TIME_BALANCE_START'})
+      expect(store.getActions()).toContainEqual({payload: true, type: 'wallet/SET_TIME_BALANCE_START'})
     })
   })
 
-  it.skip('should show 100 time deposit', () => {
+  it('should show 100 time deposit', () => {
     return store.dispatch(actions.updateTimeDeposit(account)).then(() => {
       expect(store.getActions()).toContainEqual({'payload': 100, 'type': 'wallet/SET_TIME_DEPOSIT_SUCCESS'})
     })
   })
 
-  it.skip('should show 900 time balance', () => {
+  it('should show 900 time balance', () => {
     return store.dispatch(actions.updateTimeBalance(account)).then(() => {
-      expect(store.getActions()).toContainEqual({'payload': 900, 'type': 'wallet/SET_TIME_BALANCE_SUCCESS'})
+      expect(store.getActions()).toContainEqual({'payload': 900, type: 'wallet/SET_TIME_BALANCE_SUCCESS'})
     })
   })
 
-  it.skip('should withdraw 100 time', () => {
+  it('should withdraw 100 time', () => {
     return store.dispatch(actions.withdrawTime(100, account)).then(() => {
-      expect(store.getActions()).toContainEqual({'type': 'wallet/SET_TIME_BALANCE_START'})
+      expect(store.getActions()).toContainEqual({payload: true, type: 'wallet/SET_TIME_BALANCE_START'})
     })
   })
 
   it('should show 0 time deposit #2', () => {
     return store.dispatch(actions.updateTimeDeposit(account)).then(() => {
-      expect(store.getActions()).toContainEqual({'payload': 0, 'type': 'wallet/SET_TIME_DEPOSIT_SUCCESS'})
+      expect(store.getActions()).toContainEqual({'payload': 0, type: 'wallet/SET_TIME_DEPOSIT_SUCCESS'})
     })
   })
 
-  it.skip('should show 1000 time balance #2', () => {
+  it('should show 1000 time balance #2', () => {
     return store.dispatch(actions.updateTimeBalance(account)).then(() => {
-      expect(store.getActions()).toContainEqual({'payload': 1000, 'type': 'wallet/SET_TIME_BALANCE_SUCCESS'})
+      expect(store.getActions()).toContainEqual({'payload': 1000, type: 'wallet/SET_TIME_BALANCE_SUCCESS'})
     })
   })
 
-  it.skip('should show 100000000 LHT balance of Contracts Manager', () => {
+  it('should show 0 LHT balance of Contracts Manager', () => {
     return store.dispatch(actions.updateContractsManagerLHTBalance()).then(() => {
-      expect(store.getActions()).toContainEqual({'payload': 100000000, 'type': 'wallet/SET_CONTRACTS_MANAGER_LHT_BALANCE_SUCCESS'})
+      expect(store.getActions()).toContainEqual({'payload': 0, type: 'wallet/SET_CONTRACTS_MANAGER_LHT_BALANCE_SUCCESS'})
     })
   })
 })
