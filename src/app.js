212f2ce78730c015cd6e26687e4cbf928059c69b
Pavel <pavel.dcv@gmail.com>
2017-04-24 00:47:27 +0700

    fixes for implement requested changes for PR 83

--- src/app.js
+++ src/app.js
@@ -9,14 +9,7 @@ import 'flexboxgrid/css/flexboxgrid.css'
 
 class App {
   start () {
-    window.resolveWeb3 = new Promise(resolve => {
-      window.addEventListener('load', function () {
-        resolve(window.hasOwnProperty('web3') ? window.web3 : null)
-      })
-    })
-
     injectTapEventPlugin()
-
     window.resolveWeb3.then(() => {
       render(
         <MuiThemeProvider muiTheme={themeDefault}>{require('./router.js')}</MuiThemeProvider>,
