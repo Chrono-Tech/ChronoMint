662a633e4d69eb094075dab011ec08aa87a77ade
dkchv <anton@dkchv.ru>
2017-05-03 14:36:55 +0300

    MINT-165 mnemonic validation & generation (#94)

--- specs/redux/network/actions.spec.js
+++ specs/redux/network/actions.spec.js
@@ -16,7 +16,7 @@ import { providerMap } from '../../../src/network/networkSettings'
 
 describe('network actions', () => {
   it.skip('should check TESTRPC is running', () => {
-    return store.dispatch(actions.checkTestRPC()).then(() => {
+    return store.dispatch(actions.checkTestRPC('http://localhost:8545')).then(() => {
       expect(store.getActions()[0]).toEqual({type: NETWORK_SET_TEST_RPC})
     })
   })
