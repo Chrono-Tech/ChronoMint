7f5492a999c73b31f1de1bdf57b5369b7b00f06b
Boris Shevchenko <info@bshevchenko.ru>
2017-05-05 23:36:21 +0700

    MINT-175 estimate gas fix (geth selection CORS fix remained), _call unknown function handler, txGas id added

--- src/network/Web3Utils.js
+++ src/network/Web3Utils.js
@@ -3,6 +3,7 @@ import ProviderEngine from 'web3-provider-engine'
 import WalletSubprovider from 'web3-provider-engine/subproviders/wallet'
 import Web3Subprovider from 'web3-provider-engine/subproviders/web3'
 import FilterSubprovider from 'web3-provider-engine/subproviders/filters'
+// import HttpHeaderProvider from 'httpheaderprovider' TODO MINT-175
 import Web3 from 'web3'
 
 export default class Web3Utils {
@@ -11,6 +12,10 @@ export default class Web3Utils {
     engine.addProvider(new WalletSubprovider(wallet, {}))
     engine.addProvider(new FilterSubprovider())
     engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(providerUrl)))
+    // engine.addProvider(new Web3Subprovider(new HttpHeaderProvider( TODO MINT-175
+    //   providerUrl,
+    //   {'Infura-Ethereum-Preferred-Client': 'geth'}
+    // )))
     engine.start()
     return engine
   }
