3af9c089d96a01329bc457c860e08a02bca47fa0
dkchv <anton@dkchv.ru>
2017-05-16 16:53:15 +0300

    MINT-172 wallet style fixes, not enough tokens error handling (#116)

--- src/components/pages/ExchangePage/ExchangeFormValidate.js
+++ src/components/pages/ExchangePage/ExchangeFormValidate.js
@@ -1,11 +1,23 @@
 import validator from '../../forms/validator'
 import ErrorList from '../../forms/ErrorList'
 
-export default (values) => {
+export default (values, props) => {
+  const accountBalance = props.balances[values.get('currency')]
+  const platformBalance = props.balances[values.get('currency')]
   const amount = values.get('amount')
   const amountErrors = new ErrorList()
   amountErrors.add(validator.required(amount))
   amountErrors.add(validator.currencyNumber(amount))
+  if (values.get('buy')) {
+    if (amount > platformBalance) {
+      amountErrors.add('errors.platformNotEnoughTokens')
+    }
+  } else {
+    if (amount > accountBalance) {
+      amountErrors.add('errors.notEnoughTokens')
+    }
+  }
+
   return {
     amount: amountErrors.getErrors()
   }
