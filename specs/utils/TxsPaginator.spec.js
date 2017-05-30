a8305c4a1c33735f167e0a051fc1df3cbde2e9f5
Aleksandr Arofikin <sashaaro@gmail.com>
2017-05-30 14:05:00 +0700

    fix TxsPaginator test

--- specs/utils/TxsPaginator.spec.js
+++ specs/utils/TxsPaginator.spec.js
@@ -1,3 +1,27 @@
-/**
- * Created by sasha on 30.05.17.
- */
+import TxPaginator from '../../src/utils/TxsPaginator'
+
+describe('txs paginator', () => {
+  it('should paginate', () => {
+    const paginator = new TxPaginator()
+    paginator.sizePage = 2
+
+    return Promise.all([
+      paginator.findNext(),
+      paginator.findNext(),
+      paginator.findNext()
+    ])
+    .then(([txs, txs2, txs3]) => {
+      expect(txs.length).toEqual(2)
+      expect(txs2.length).toEqual(2)
+      expect(txs3.length).toEqual(2)
+
+      const paginator = new TxPaginator()
+      paginator.sizePage = 6
+
+      return paginator.findNext().then((allTxs) => {
+        expect(txs.concat(txs2).concat(txs3).sort()).toEqual(allTxs.sort())
+      })
+    })
+  })
+})
+
