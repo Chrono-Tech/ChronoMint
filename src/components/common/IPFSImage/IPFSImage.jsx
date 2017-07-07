76c453551eebee9a2ab535963512c593df6e6be8
Anton Dokuchaev <anton@dkchv.ru>
2017-07-07 15:10:48 +0300

    MINT-192 fix avatar bg

--- src/components/common/IPFSImage/IPFSImage.jsx
+++ src/components/common/IPFSImage/IPFSImage.jsx
@@ -10,13 +10,14 @@ export default class IPFSImage extends React.Component {
   static propTypes = {
     multihash: PropTypes.string,
     fallback: PropTypes.string,
-    className: PropTypes.string
+    className: PropTypes.string,
+    icon: PropTypes.object
   }
 
   constructor (props) {
     super(props)
     this.state = {
-      image: null,
+      imageURL: null
     }
   }
 
@@ -31,12 +32,11 @@ export default class IPFSImage extends React.Component {
   }
 
   async loadImage (multihash) {
-
     try {
       const image = multihash && await IPFS.get(multihash)
       if (image) {
         this.setState({
-          image: image.data
+          imageURL: image.data
         })
       }
     } catch (e) {
@@ -46,10 +46,16 @@ export default class IPFSImage extends React.Component {
   }
 
   render () {
+    const {icon} = this.props
+    const {imageURL} = this.state || this.props.fallback
+
     return (
-      <div styleName='root' className={this.props.className}
-           style={{backgroundImage: `url("${this.state.image || this.props.fallback}")`}}
-      ></div>
-    )
+        <div
+          styleName='root'
+          className={this.props.className}
+          style={{backgroundImage: `url("${imageURL}")`}}
+        >{!imageURL && icon}</div>
+      )
+
   }
 }
