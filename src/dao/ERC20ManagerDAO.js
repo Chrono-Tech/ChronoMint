e124ab293d5eee969c4f74bc4010acf4616277eb
Boris Shevchenko <info@bshevchenko.ru>
2017-08-08 09:11:37 +0700

    contracts 1.1.35 and get user tokens bug fix

--- src/dao/ERC20ManagerDAO.js
+++ src/dao/ERC20ManagerDAO.js
@@ -93,8 +93,9 @@ export default class ERC20ManagerDAO extends AbstractContractDAO {
 
     // get balances
     promises = []
-    for (let dao of daos) {
-      promises.push(dao.getAccountBalance('pending'))
+    for (let i of Object.keys(tokensAddresses)) {
+      this.initTokenMetaData(daos[i], symbols[i], decimalsArr[i])
+      promises.push(daos[i].getAccountBalance('latest'))
     }
     const balances = await Promise.all(promises)
 
@@ -106,7 +107,7 @@ export default class ERC20ManagerDAO extends AbstractContractDAO {
       const ethToken = new TokenModel({
         dao: ethereumDAO,
         name: EthereumDAO.getName(),
-        balance: await ethereumDAO.getAccountBalance('pending')
+        balance: await ethereumDAO.getAccountBalance('latest')
       })
       map = map.set(ethToken.id(), ethToken)
     }
@@ -114,7 +115,6 @@ export default class ERC20ManagerDAO extends AbstractContractDAO {
     const timeHolderAddress = timeHolderDAO.getInitAddress()
 
     for (let [i, address] of Object.entries(tokensAddresses)) {
-      this.initTokenMetaData(daos[i], symbols[i], decimalsArr[i])
       const token = new TokenModel({
         address,
         dao: daos[i],
