efd1bd80f47b9a1a24413b9387ea5446658b913e
Anton Dokuchaev <anton@dkchv.ru>
2017-08-17 16:23:32 +0300

    MINT-416 unlock setInterval

--- src/redux/market/action.js
+++ src/redux/market/action.js
@@ -2,28 +2,29 @@ export const MARKET_INIT = 'market/INIT'
 export const MARKET_ADD_TOKEN = 'market/ADD_TOKEN'
 export const MARKET_UPDATE_PRICES = 'market/UPDATE_PRICES'
 
-const MARKET_REQUEST_DELAY = 1000
+const MARKET_REQUEST_DELAY = 30000
 let timerId
 
-const watchMarket = (dispatch, getState) => () => {
-  // TODO @dkchv: !!!
-  clearInterval(timerId)
-  const market = getState().get('market')
-  const tokens = market.tokens.join(',')
-  const currencies = market.currencies.join(',')
-  if (!tokens || !currencies) {
+const watchMarket = (dispatch, getState) => async () => {
+  const {tokens, currencies } = getState().get('market')
+  if (tokens.length === 0 || !currencies.length === 0) {
     return
   }
-  fetch(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${tokens}&tsyms=${currencies}`).then(response => {
-    return response ? response.json() : {}
-  }).then(prices => {
-    dispatch({type: MARKET_UPDATE_PRICES, prices})
-  })
+  const response = await fetch(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${tokens.join(',')}&tsyms=${currencies.join(',')}`)
+  const prices = response ? await response.json() : {}
+  dispatch({type: MARKET_UPDATE_PRICES, prices})
 }
 
-export const watchInitMarket = () => (dispatch, getState) => {
-  timerId = setInterval(watchMarket(dispatch, getState), MARKET_REQUEST_DELAY)
-  dispatch({type: MARKET_INIT, isInited: true})
+export const watchInitMarket = () => async (dispatch, getState) => {
+  try {
+    watchMarket(dispatch, getState)()
+    timerId = setInterval(watchMarket(dispatch, getState), MARKET_REQUEST_DELAY)
+    dispatch({type: MARKET_INIT, isInited: true})
+  } catch (e) {
+    // eslint-disable-next-line
+    console.error('init market error', e)
+    dispatch({type: MARKET_INIT, isInited: false})
+  }
 }
 
 export const watchStopMarket = () => (dispatch) => {
