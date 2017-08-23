f064bc7f074a2fd81e15990bf4e8f6799700031f
Oleg Dizus <olegdizus@gmail.com>
2017-08-23 12:54:28 +0300

    MINT-430 Delete all 'beta' labels

--- src/layouts/Splash/Splash.js
+++ src/layouts/Splash/Splash.js
@@ -12,7 +12,7 @@ class Splash extends Component {
     return (
       <div styleName='root'>
         <div styleName='content'>
-          <ChronoBankLogo version='beta v0.1.2' />
+          <ChronoBankLogo version='v0.1.2' />
           {this.props.children}
         </div>
         <div styleName='footer'>
