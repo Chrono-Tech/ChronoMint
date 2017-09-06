fa0b9f294ed8c6c291c83156bc7ef5655525dacd
Oleg Dizus <olegdizus@gmail.com>
2017-09-06 13:23:03 +0300

    React Native connector for WebView messages

---
+++ src/connectReactNative.js
@@ -0,0 +1,36 @@
+/**
+ * This singleton simplify and promisify React Native WewView messages flow.
+ * It has postMessage method that accepts message name and payload and
+ * returns a promise. The returned promise is resolving or rejecting
+ * when answer from React Native was received.
+ */
+class ConnectReactNative {
+  constructor () {
+    this.handlers = {}
+    
+    document.addEventListener('message', ({ data }) => {
+      try {
+        const { message, error, ...payload } = JSON.parse(data)
+        const handler = this.handlers[message] 
+
+        handler && (
+          error ?
+            handler.reject(error) :
+            handler.resolve(payload)
+        )
+      } catch (e) { return }
+    })
+  }
+
+  postMessage = async (message, payload) => {
+    const handler = new Promise((resolve, reject) => {
+      this.handlers[message] = { resolve, reject }
+    })
+    
+    await window.postMessage(JSON.stringify({ message, ...payload }))
+
+    return await handler
+  }
+}
+
+export default new ConnectReactNative()
