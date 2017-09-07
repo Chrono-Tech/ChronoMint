c701df595cc7399b2655d63b1f807839e7ac1fd6
Anton Dokuchaev <anton@dkchv.ru>
2017-09-07 15:34:07 +0300

    MINT-453 fix tests

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -1,20 +1,21 @@
-import type BigNumber from 'bignumber.js'
+import BigNumber from 'bignumber.js'
 
 /**
  * Convert number 24123432 into string '24 123 432' with nbsp-spaces.
  * Useful in renders
  * \u00a0 = &nbsp;
  */
-export function integerWithDelimiter (value: BigNumber, withFraction = false): string {
-  if (value.isZero()) {
+export function integerWithDelimiter (value: any, withFraction = false): string {
+  const valueBN = new BigNumber(value || 0)
+  if (valueBN.isZero() || valueBN.isNaN()) {
     return withFraction ? '0.00' : '0'
   }
 
   if (withFraction) {
-    return value.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1\u00a0')
+    return valueBN.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1\u00a0')
   } else {
-    const roundedValue = value.lt(0) ? value.ceil() : value.floor()
-    const sign = value.lt(0) ? '-': ''
+    const roundedValue = valueBN.lt(0) ? valueBN.ceil() : valueBN.floor()
+    const sign = valueBN.lt(0) ? '-': ''
     return sign + roundedValue.toFixed(0).replace(/(\d)(?=(\d{3})+$)/g, '$1\u00a0')
   }
 }
