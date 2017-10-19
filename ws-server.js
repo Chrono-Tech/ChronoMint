9b1320db6d251229242dffda2729449682306845
Vyacheslav Frolov <algebris@gmail.com>
2017-10-19 12:26:43 +0300

    fix HTTP request
    MINT-626

--- ws-server.js
+++ ws-server.js
@@ -6,35 +6,16 @@ const request = require('request')
 const server = stubServer.createStubServer('WS', 'ws://localhost:8546')
 
 server.addResponder((requestJso) => {
-  const postData = querystring.stringify(requestJso)
+  const postData = JSON.stringify(requestJso)
 
   const options = {
     hostname: 'localhost',
     port: 8545,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Connection': 'keep-alive',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Content-Length': Buffer.byteLength(postData)
-    }
+    form: postData
   }
-
-  const req = http.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`)
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
-    res.setEncoding('utf8')
-    res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`)
-    })
-
-  })
-
-  req.on('error', (e) => {
-    // eslint-disable-next-line
-    console.error('error', e.message)
-  })
-
-  req.write(postData)
-  return req.end()
+  
+  request.post('http://localhost:8545', options, (err,httpResponse,body) => {
+    if(err) console.error(err)
+    console.log('body', body)
+  });
 })
