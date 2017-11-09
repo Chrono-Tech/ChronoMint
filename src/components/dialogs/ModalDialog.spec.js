bb8cd01d64c3c919be630461976565d7ced8cba2
Andrey Antropov <q.laiff@gmail.com>
2017-11-10 00:16:57 +0500

    Fix ModalDialog tests

--- src/components/dialogs/ModalDialog.spec.js
+++ src/components/dialogs/ModalDialog.spec.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import { mount, shallow } from 'enzyme'
+import Immutable from 'immutable'
+import { Provider } from 'react-redux'
+import { mockStore } from 'specsInit'
+import { mount } from 'enzyme'
 import ModalDialog from 'components/dialogs/ModalDialog'
 
 injectTapEventPlugin()
 
 describe('ModalDialog', () => {
   it('should mount and render content', () => {
-    const wrapper = mount(<ModalDialog>
-      <div>Foo</div>
-      <div>Bar</div>
-    </ModalDialog>)
+    const store = mockStore(Immutable.Map({}))
+    const wrapper = mount(
+      <Provider store={store}>
+        <ModalDialog>
+          <div>Foo</div>
+          <div>Bar</div>
+        </ModalDialog>
+      </Provider>
+    )
 
     expect(wrapper.find('.ModalDialog__backdrop').length).toEqual(1)
     expect(wrapper.find('.ModalDialog__backdrop > .ModalDialog__dialog').length).toEqual(1)
