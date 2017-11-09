e7b69d2286d067f559fc877b8b08c5a1cb01f9e9
Anton Dokuchaev <anton@dkchv.ru>
2017-11-09 19:28:50 +0300

    MINT-687 fix poll form

--- src/components/dialogs/poll/PollEditDialog/PollEditForm.js
+++ src/components/dialogs/poll/PollEditDialog/PollEditForm.js
@@ -1,14 +1,18 @@
+import BigNumber from 'bignumber.js'
+import classnames from 'classnames'
 import FileSelect from 'components/common/FileSelect/FileSelect'
-import { RaisedButton } from 'material-ui'
+import { FlatButton, FontIcon, IconButton, RaisedButton } from 'material-ui'
 import { ACCEPT_DOCS } from 'models/FileSelect/FileExtension'
+import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Translate } from 'react-redux-i18n'
 import { DatePicker, TextField } from 'redux-form-material-ui'
 import { Field, FieldArray, formPropTypes, formValueSelector, reduxForm } from 'redux-form/immutable'
-import { DUCK_SESSION } from 'redux/session/actions'
-import { DUCK_VOTING } from 'redux/voting/actions'
 import { DUCK_I18N } from 'redux/configureStore'
+import { modalsClose } from 'redux/modals/actions'
+import { DUCK_SESSION } from 'redux/session/actions'
+import { createPoll, DUCK_VOTING, updatePoll } from 'redux/voting/actions'
 import './PollEditForm.scss'
 import validate from './validate'
 
@@ -20,6 +24,7 @@ function prefix (token) {
 
 function mapStateToProps (state) {
   const selector = formValueSelector(FORM_EDIT_POLL)
+
   return {
     options: selector(state, 'options'),
     account: state.get(DUCK_SESSION).account,
@@ -45,15 +50,80 @@ function mapDispatchToProps (dispatch, props) {
   }
 }
 
-const onSubmit = (values) => {
-  console.log('--PollEditForm#onSubmit', values.toJS())
-}
-
 @connect(mapStateToProps, mapDispatchToProps)
-@reduxForm({ form: FORM_EDIT_POLL, validate, onSubmit })
+@reduxForm({ form: FORM_EDIT_POLL, validate })
 export default class PollEditForm extends Component {
   static propTypes = {
+    isModify: PropTypes.bool,
+    account: PropTypes.string,
+    voteLimit: PropTypes.objectOf(BigNumber),
+    locale: PropTypes.string,
     ...formPropTypes,
+  }
+
+  constructor () {
+    super(...arguments)
+    this.state = {
+      selectedOptionIndex: 0,
+    }
+  }
+
+  handleOptionSelect (index) {
+    this.setState({ selectedOptionIndex: index })
+  }
+
+  handleOptionCreate (options) {
+    options.push()
+    this.setState({ selectedOptionIndex: options.length })
+  }
+
+  handleOptionRemove (options, index) {
+    options.remove(index)
+    if (this.state.selectedOptionIndex >= options.length) {
+      this.setState({ selectedOptionIndex: options.length - 1 })
+    }
+  }
+
+  renderOptions (options) {
+    const optionsList = options.getAll()
+    return (
+      <div>
+        <div styleName='optionsActions'>
+          <FlatButton
+            label={<Translate value={prefix('addOption')} />}
+            styleName='optionsAction'
+            onTouchTap={() => this.handleOptionCreate(options)}
+          />
+        </div>
+        <div styleName='optionsList'>
+          <div styleName='listTable'>
+            {optionsList && optionsList.toArray().map((option, index) => (
+              <div
+                key={index}
+                styleName={classnames('tableItem', { active: this.state.selectedOptionIndex === index })}
+                onTouchTap={() => this.handleOptionSelect(index)}
+              >
+                <div styleName='itemLeft'>
+                  <div styleName='symbol symbolFill'>#{index + 1}</div>
+                </div>
+                <div styleName='itemMain'>
+                  <div styleName='mainTitle'><Translate value={prefix('optionIndex')} index={index + 1} /></div>
+                  <div styleName='mainOption'>{option}</div>
+                </div>
+                <div styleName='itemRight'>
+                  <IconButton>
+                    <FontIcon className='material-icons'>mode_edit</FontIcon>
+                  </IconButton>
+                  <IconButton>
+                    <FontIcon className='material-icons' onTouchTap={() => this.handleOptionRemove(options, index)}>delete</FontIcon>
+                  </IconButton>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    )
   }
 
   render () {
--- src/components/dialogs/poll/PollEditDialog/validate.js
+++ src/components/dialogs/poll/PollEditDialog/validate.js
@@ -2,8 +2,8 @@ import ErrorList from 'components/forms/ErrorList'
 import validator from 'components/forms/validator'
 
 export default function validate (values) {
-  const errors = {}
-  errors.title = ErrorList.toTranslate(validator.required(values.get('title')))
-  errors.files = ErrorList.toTranslate(validator.validIpfsFileList(values.get('files')))
-  return errors
+  return {
+    title: ErrorList.toTranslate(validator.required(values.get('title'))),
+    files: ErrorList.toTranslate(validator.validIpfsFileList(values.get('files'))),
+  }
 }
