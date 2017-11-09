3b70a67c93228a69f7f85f635cc819de3395e509
Andrey Antropov <q.laiff@gmail.com>
2017-11-09 23:45:49 +0500

    Fix tests for network and ModalStack

--- src/Login/network/nem/index.js
+++ src/Login/network/nem/index.js
@@ -1,3 +1,3 @@
-export KeyPair from './lib/KeyPair'
-export Address from './lib/Address'
-export Network from './lib/Network'
+export { default as KeyPair } from './lib/KeyPair'
+export { default as Address } from './lib/Address'
+export { default as Network } from './lib/Network'
