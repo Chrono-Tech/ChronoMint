ef049596d43414725acc6b3e622d2cc00a00273a
Abdulov Ivan <abdulov.is.work@gmail.com>
2017-11-29 15:49:25 +0300

    MINT-814 add serializator with sort for tests (#408)

--- src/redux/assetsManager/reducer.spec.js
+++ src/redux/assetsManager/reducer.spec.js
@@ -5,141 +5,112 @@ import reducer, { initialState } from './reducer'
 
 describe('assetsManager reducer', () => {
   it('should return initial state', () => {
-    expect(reducer(
-      undefined,
-      {}
-    ))
-      .toEqual(initialState)
+    expect(reducer(undefined, {})).toEqual(initialState)
   })
 
   it('should handle GET_ASSETS_MANAGER_COUNTS_START', () => {
-    expect(reducer(
-      null,
-      {
+    expect(
+      reducer(null, {
         type: a.GET_ASSETS_MANAGER_COUNTS_START,
-      }))
-      .toEqual({
-        assetsManagerCountsLoading: true,
-      })
+      }),
+    ).toEqual({
+      assetsManagerCountsLoading: true,
+    })
   })
 
   it('should handle GET_ASSETS_MANAGER_COUNTS', () => {
-    expect(reducer(
-      null,
-      {
+    expect(
+      reducer(null, {
         type: a.GET_ASSETS_MANAGER_COUNTS,
         payload: {
           assets: { a: '1' },
-          managers: [1, 2, 3, 4],
-          platforms:
-            [1, 2, 3, 4],
+          managers: [ 'test', 'test1', 'test2' ],
+          platforms: [ 'test', 'test1', 'test2' ],
         },
-      }))
-      .toEqual({
-        assetsManagerCountsLoading: false,
-        tokensCount: 1,
-        managersCount: 4,
-        assets: { a: '1' },
-        managersList: [1, 2, 3, 4],
-        platformsList: [1, 2, 3, 4],
-      })
+      }),
+    ).toMatchSnapshot()
   })
 
   it('should handle GET_PLATFORMS', () => {
-    expect(reducer(
-      null,
-      {
+    expect(
+      reducer(null, {
         type: a.GET_PLATFORMS,
         payload: {
-          platforms: [1, 2, 3, 4],
+          platforms: [ 'test', 'test1', 'test2' ],
         },
-      }))
-      .toEqual({
-        platformsList: [1, 2, 3, 4],
-      })
+      }),
+    ).toMatchSnapshot()
   })
 
   it('should handle GET_MANAGERS_FOR_TOKEN_LOADING', () => {
-    expect(reducer(
-      null,
-      {
+    expect(
+      reducer(null, {
         type: a.GET_MANAGERS_FOR_TOKEN_LOADING,
-      }))
-      .toEqual({
-        managersForTokenLoading: true,
-      })
+      }),
+    ).toEqual({
+      managersForTokenLoading: true,
+    })
   })
 
   it('should handle SELECT_TOKEN', () => {
-    expect(reducer(
-      null,
-      {
+    expect(
+      reducer(null, {
         type: a.SELECT_TOKEN,
         payload: {
           symbol: 'LHT',
         },
-      }))
-      .toEqual({
-        selectedToken: 'LHT',
-      })
+      }),
+    ).toEqual({
+      selectedToken: 'LHT',
+    })
   })
 
   it('should handle SELECT_PLATFORM', () => {
-    expect(reducer(
-      null,
-      {
+    expect(
+      reducer(null, {
         type: a.SELECT_PLATFORM,
         payload: {
           platformAddress: 'test',
         },
-      })).toEqual({
+      }),
+    ).toEqual({
       selectedPlatform: 'test',
     })
   })
 
   it('should handle GET_TOKENS', () => {
     const map = new Immutable.Map({ a: 1, b: 2 })
-    expect(reducer(
-      null,
-      {
-        type: a.GET_TOKENS,
-        payload: {
-          tokensMap: map,
-          assets: [1, 2, 3, 4],
-        },
-      }))
-      .toEqual({
-        tokensCount: 2,
+    const result = reducer(null, {
+      type: a.GET_TOKENS,
+      payload: {
         tokensMap: map,
-        assets: [1, 2, 3, 4],
-      })
+        assets: [ 1, 2, 3, 4 ],
+      },
+    })
+    expect(result).toMatchSnapshot()
   })
 
   it('should handle SET_TOKEN', () => {
-    const map = new Immutable.Map({ 'aaa': new TokenModel(), 'bbb': new TokenModel() })
-    const map1 = new Immutable.Map({ 'ccc': new TokenModel() })
-    const mapRes = new Immutable.Map({ 'aaa': new TokenModel(), 'bbb': new TokenModel(), 'ccc': new TokenModel() })
-    expect(reducer(
+    const map = new Immutable.Map({ aaa: new TokenModel(), bbb: new TokenModel() })
+    const mapAciont = new Immutable.Map({ ccc: new TokenModel() })
+    const result = reducer(
       {
         tokensMap: map,
       },
       {
         type: a.SET_TOKEN,
         payload: {
-          tokensMap: map1,
-          assets: [1, 2],
+          tokensMap: mapAciont,
+          assets: [ 1, 2 ],
         },
-      }))
-      .toEqual({
-        tokensCount: 3,
-        tokensMap: mapRes,
-        assets: [1, 2],
-      })
+      },
+    )
+    expect(result).toMatchSnapshot()
   })
 
   it('should handle GET_MANAGERS_FOR_TOKEN', () => {
-    const map = new Immutable.Map({ 'LHT': new TokenModel({ 'managersList': [1] }) })
-    expect(reducer(
+    const map = new Immutable.Map({ LHT: new TokenModel({ managersList: [ 1 ] }) })
+    const result = reducer(
       {
         tokensMap: map,
       },
@@ -147,22 +118,20 @@ describe('assetsManager reducer', () => {
         type: a.GET_MANAGERS_FOR_TOKEN,
         payload: {
           symbol: 'LHT',
-          managersForAssetSymbol: [1, 2, 3],
+          managersForAssetSymbol: [ 1, 2, 3 ],
         },
-      }))
-      .toEqual({
-        tokensMap: new Immutable.Map({ 'LHT': new TokenModel({ 'managersList': [1, 2, 3] }) }),
-        managersForTokenLoading: false,
-      })
+      },
+    )
+    expect(result).toMatchSnapshot()
   })
 
   it('should handle SET_TOTAL_SUPPLY', () => {
     const token = new TokenModel({ symbol: 'LHT', address: '1234', totalSupply: 5 })
-    expect(reducer(
+    const result = reducer(
       {
-        tokensMap: new Immutable.Map({ 'LHT': token }),
+        tokensMap: new Immutable.Map({ LHT: token }),
         assets: {
-          '1234': {
+          test: {
             totalSupply: 1,
           },
         },
@@ -172,22 +141,16 @@ describe('assetsManager reducer', () => {
         payload: {
           token: token,
         },
-      }))
-      .toEqual({
-        'assets': {
-          '1234': {
-            totalSupply: 5,
-          },
-        },
-        tokensMap: new Immutable.Map({ 'LHT': token }),
-      })
+      },
+    )
+    expect(result).toMatchSnapshot()
   })
 
   it('should handle SET_IS_REISSUABLE', () => {
     const token = new TokenModel({ symbol: 'LHT' })
-    expect(reducer(
+    const result = reducer(
       {
-        tokensMap: new Immutable.Map({ 'LHT': token }),
+        tokensMap: new Immutable.Map({ LHT: token }),
       },
       {
         type: a.SET_IS_REISSUABLE,
@@ -195,17 +158,16 @@ describe('assetsManager reducer', () => {
           isReissuable: true,
           symbol: 'LHT',
         },
-      }))
-      .toEqual({
-        tokensMap: new Immutable.Map({ 'LHT': token.isReissuable(true) }),
-      })
+      },
+    )
+    expect(result).toMatchSnapshot()
   })
 
   it('should handle SET_FEE', () => {
     const token = new TokenModel({ symbol: 'LHT' })
-    expect(reducer(
+    const result = reducer(
       {
-        tokensMap: new Immutable.Map({ 'LHT': token }),
+        tokensMap: new Immutable.Map({ LHT: token }),
       },
       {
         type: a.SET_FEE,
@@ -214,74 +176,61 @@ describe('assetsManager reducer', () => {
           withFee: true,
           symbol: 'LHT',
         },
-      }))
-      .toEqual({
-        tokensMap: new Immutable.Map({ 'LHT': token.fee(1).withFee(true) }),
-      })
+      },
+    )
+    expect(result).toMatchSnapshot()
   })
 
   it('should handle SET_NEW_MANAGERS_LIST', () => {
     const token = new TokenModel({ symbol: 'LHT' })
-    expect(reducer(
+    const result = reducer(
       {
-        tokensMap: new Immutable.Map({ 'LHT': token }),
+        tokensMap: new Immutable.Map({ LHT: token }),
       },
       {
         type: a.SET_NEW_MANAGERS_LIST,
         payload: {
-          managers: [1, 2, 3],
-          managersList: [1, 2, 3],
+          managers: [ 'test', 'test1', 'test2' ],
+          managersList: [ 'test', 'test1', 'test2' ],
           symbol: 'LHT',
         },
-      }))
-      .toEqual({
-        tokensMap: new Immutable.Map({ 'LHT': token.managersList([1, 2, 3]) }),
-        managersList: [1, 2, 3],
-        managersCount: 3,
-      })
+      },
+    )
+    expect(result).toMatchSnapshot()
   })
 
   it('should handle GET_TRANSACTIONS_START', () => {
-    expect(reducer(
-      null,
-      {
+    expect(
+      reducer(null, {
         type: a.GET_TRANSACTIONS_START,
-      }))
-      .toEqual({
-        transactionsFetching: true,
-      })
+      }),
+    ).toEqual({
+      transactionsFetching: true,
+    })
   })
 
   it('should handle GET_TRANSACTIONS_DONE', () => {
-    expect(reducer(
-      {
-        transactionsList: [],
-      },
-      {
-        type: a.GET_TRANSACTIONS_DONE,
-        payload: {
-          transactionsList: [1, 2, 3, 4, 5],
+    expect(
+      reducer(
+        { transactionsList: [] },
+        {
+          type: a.GET_TRANSACTIONS_DONE,
+          payload: {
+            transactionsList: [ { txHash: 'test' }, { txHash: 'test1' } ],
+          },
         },
-      }))
-      .toEqual({
-        transactionsList: [1, 2, 3, 4, 5],
-        transactionsFetched: true,
-        transactionsFetching: false,
-      })
+      ),
+    ).toMatchSnapshot()
   })
 
   it('should handle GET_USER_PLATFORMS', () => {
-    expect(reducer(
-      null,
-      {
+    expect(
+      reducer(null, {
         type: a.GET_USER_PLATFORMS,
         payload: {
-          usersPlatforms: [1, 2, 3, 4, 5],
+          usersPlatforms: [ 'test', 'test1', 'test2' ],
         },
-      }))
-      .toEqual({
-        usersPlatforms: [1, 2, 3, 4, 5],
-        usersPlatformsCount: 5,
-      })
+      }),
+    ).toMatchSnapshot()
   })
 })
