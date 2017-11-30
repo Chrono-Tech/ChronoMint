62b23957e1b7727b8693f2003a5e992bb56b512e
AIS <abdulov001@gmail.com>
2017-11-30 14:33:18 +0300

    Small fix

--- src/components/exchange/AddExchangeDialog/AddExchangeDialog.jsx
+++ src/components/exchange/AddExchangeDialog/AddExchangeDialog.jsx
@@ -1,32 +1,20 @@
 import ModalDialog from 'components/dialogs/ModalDialog'
 import ExchangeOrderModel from 'models/exchange/ExchangeOrderModel'
-import PropTypes from 'prop-types'
 import React, { PureComponent } from 'react'
-import { connect } from 'react-redux'
 import { createExchange } from 'redux/exchange/actions'
 import { modalsClose } from 'redux/modals/actions'
 import AddExchangeForm from './AddExchangeForm'
 
-function mapDispatchToProps (dispatch) {
-  return {
-    onSubmitSuccess: (exchange: ExchangeOrderModel) => {
-      dispatch(createExchange(exchange))
-      dispatch(modalsClose())
-    },
-  }
+const onSubmitSuccess = (exchange: ExchangeOrderModel, dispatch) => {
+  dispatch(createExchange(exchange))
+  dispatch(modalsClose())
 }
 
-@connect(null, mapDispatchToProps)
 export default class AddExchangeDialog extends PureComponent {
-  static propTypes = {
-    onSubmitSuccess: PropTypes.func,
-    closeModal: PropTypes.func,
-  }
-
   render () {
     return (
       <ModalDialog>
-        <AddExchangeForm onSubmitSuccess={this.props.onSubmitSuccess} />
+        <AddExchangeForm onSubmitSuccess={onSubmitSuccess} />
       </ModalDialog>
     )
   }
