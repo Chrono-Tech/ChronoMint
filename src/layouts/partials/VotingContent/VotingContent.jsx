3ce7f829606a34f53a2aa41d9ad69131eba37245
Abdulov Ivan <abdulov.is.work@gmail.com>
2017-12-01 14:14:40 +0300

    MINT-827 rework voting page (#413)

--- src/layouts/partials/VotingContent/VotingContent.jsx
+++ src/layouts/partials/VotingContent/VotingContent.jsx
@@ -2,7 +2,7 @@ import BigNumber from 'bignumber.js'
 import { Link } from 'react-router'
 import { Poll, PollEditDialog } from 'components'
 import PropTypes from 'prop-types'
-import { RaisedButton, CircularProgress } from 'material-ui'
+import { RaisedButton } from 'material-ui'
 import React, { Component } from 'react'
 import { Translate } from 'react-redux-i18n'
 import { connect } from 'react-redux'
@@ -12,6 +12,7 @@ import { DUCK_MAIN_WALLET, initTIMEDeposit } from 'redux/mainWallet/actions'
 import { DUCK_VOTING, listPolls } from 'redux/voting/actions'
 import { modalsOpen } from 'redux/modals/actions'
 import { DUCK_SESSION } from 'redux/session/actions'
+import Preloader from 'components/common/Preloader/Preloader'
 
 import './VotingContent.scss'
 
@@ -69,7 +70,7 @@ export default class VotingContent extends Component {
   }
 
   renderHead () {
-    const { statistics } = this.props
+    const { statistics, isFetching } = this.props
 
     return (
       <div styleName='head'>
@@ -84,17 +85,19 @@ export default class VotingContent extends Component {
                       <i className='material-icons'>poll</i>
                     </div>
                     <div styleName='entry'>
-                      <span styleName='entry1'><Translate value={prefix('allPolls')} />:</span><br />
-                      <span styleName='entry2'>{statistics.all}</span>
+                      <span styleName='entryTitle'><Translate value={prefix('allPolls')} />:</span><br />
+                      <span styleName='entryValue'>{isFetching ? <Preloader /> : statistics.all}</span>
                     </div>
                   </div>
+                </div>
+                <div styleName='contentStats'>
                   <div styleName='contentStatsItem statsCompleted'>
                     <div styleName='icon'>
                       <i className='material-icons'>check</i>
                     </div>
                     <div styleName='entry'>
-                      <span styleName='entry1'><Translate value={prefix('completedPolls')} />:</span><br />
-                      <span styleName='entry2'>{statistics.completed}</span>
+                      <span styleName='entryTitle'><Translate value={prefix('completedPolls')} />:</span><br />
+                      <span styleName='entryValue'>{isFetching ? <Preloader /> : statistics.completed}</span>
                     </div>
                   </div>
                   <div styleName='contentStatsItem statsOutdated'>
@@ -102,17 +105,19 @@ export default class VotingContent extends Component {
                       <i className='material-icons'>event_busy</i>
                     </div>
                     <div styleName='entry'>
-                      <span styleName='entry1'><Translate value={prefix('outdatedPolls')} />:</span><br />
-                      <span styleName='entry2'>{statistics.outdated}</span>
+                      <span styleName='entryTitle'><Translate value={prefix('outdatedPolls')} />:</span><br />
+                      <span styleName='entryValue'>{isFetching ? <Preloader /> : statistics.outdated}</span>
                     </div>
                   </div>
+                </div>
+                <div styleName='contentStats'>
                   <div styleName='contentStatsItem statsInactive'>
                     <div styleName='icon'>
                       <i className='material-icons'>error_outline</i>
                     </div>
                     <div styleName='entry'>
-                      <span styleName='entry1'><Translate value={prefix('inactivePolls')} />:</span><br />
-                      <span styleName='entry2'>{statistics.inactive}</span>
+                      <span styleName='entryTitle'><Translate value={prefix('inactivePolls')} />:</span><br />
+                      <span styleName='entryValue'>{isFetching ? <Preloader /> : statistics.inactive}</span>
                     </div>
                   </div>
                   <div styleName='contentStatsItem statsOngoing'>
@@ -120,8 +125,8 @@ export default class VotingContent extends Component {
                       <i className='material-icons'>access_time</i>
                     </div>
                     <div styleName='entry'>
-                      <span styleName='entry1'><Translate value={prefix('pollsOngoing')} />:</span><br />
-                      <span styleName='entry2'>{statistics.ongoing}</span>
+                      <span styleName='entryTitle'><Translate value={prefix('pollsOngoing')} />:</span><br />
+                      <span styleName='entryValue'>{isFetching ? <Preloader /> : statistics.ongoing}</span>
                     </div>
                   </div>
                 </div>
@@ -131,9 +136,10 @@ export default class VotingContent extends Component {
                   <div styleName='entries' />
                   <div>
                     <RaisedButton
+                      disabled={this.props.timeDeposit.equals(new BigNumber(0))}
                       label={<Translate value={prefix('newPoll')} />}
                       styleName='action'
-                      onTouchTap={() => this.props.handleNewPoll()}
+                      onClick={this.props.handleNewPoll}
                     />
                   </div>
                 </div>
@@ -153,7 +159,10 @@ export default class VotingContent extends Component {
             <div className='row'>
               {polls.map((poll) => (
                 <div className='col-sm-6 col-md-3' key={poll.poll().id()}>
-                  <Poll model={poll} />
+                  <Poll
+                    model={poll}
+                    timeDeposit={this.props.timeDeposit}
+                  />
                 </div>
               ))}
             </div>
@@ -164,25 +173,6 @@ export default class VotingContent extends Component {
   }
 
   render () {
-    if (!this.props.isFetched) {
-      return (
-        <div styleName='progress'>
-          <CircularProgress size={24} thickness={1.5} />
-        </div>
-      )
-    }
-
-    if (this.props.timeDeposit.equals(new BigNumber(0))) {
-      return (
-        <div styleName='root'>
-          <div styleName='content'>
-            <div styleName='accessDenied'>
-              <i className='material-icons' styleName='accessDeniedIcon'>warning</i>Deposit TIME on <Link to='/wallet'>Wallet page</Link> if you want get access this page.
-            </div>
-          </div>
-        </div>
-      )
-    }
 
     const polls = this.props.isFetched
       ? this.props.list.reverse().toArray()
@@ -192,7 +182,14 @@ export default class VotingContent extends Component {
       <div styleName='root'>
         <div styleName='content'>
           {this.renderHead(polls)}
-          {this.renderBody(polls)}
+          {this.props.isFetched && this.props.timeDeposit.equals(new BigNumber(0)) &&
+          (
+            <div styleName='accessDenied'>
+              <i className='material-icons' styleName='accessDeniedIcon'>warning</i>Deposit TIME on <Link
+              to='/wallet'>Wallet page</Link> if you want get access this page.
+            </div>
+          )}
+          {this.props.isFetched && this.renderBody(polls)}
         </div>
       </div>
     )
