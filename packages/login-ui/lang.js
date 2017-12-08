bec90552950045fa8862e7c0deb55c2635f9bc32
Andrew Laiff <q.laiff@gmail.com>
2017-12-08 13:46:18 +0500

    Mint 794 fix login-ui (#421)

--- packages/login-ui/lang.js
+++ packages/login-ui/lang.js
@@ -2,22 +2,22 @@ import * as AccountSelector from './components/AccountSelector/lang'
 import * as BackButton from './components/BackButton/lang'
 import * as GenerateMnemonic from './components/GenerateMnemonic/lang'
 import * as GenerateWallet from './components/GenerateWallet/lang'
+import * as LoginForm from './components/LoginForm/lang'
 import * as LoginMetamask from './components/LoginMetamask/lang'
 import * as LoginUPort from './components/LoginUPort/lang'
 import * as LoginWithLedger from './components/LoginWithLedger/lang'
-import * as LoginWithTrezor from './components/LoginWithTrezor/lang'
 import * as LoginWithMnemonic from './components/LoginWithMnemonic/lang'
 import * as LoginWithOptions from './components/LoginWithOptions/lang'
 import * as LoginWithPrivateKey from './components/LoginWithPrivateKey/lang'
+import * as LoginWithTrezor from './components/LoginWithTrezor/lang'
 import * as LoginWithWallet from './components/LoginWithWallet/lang'
 import * as NetworkSelector from './components/NetworkSelector/lang'
 import * as ProviderSelector from './components/ProviderSelector/lang'
 import * as ProviderSelectorSwitcher from './components/ProviderSelectorSwitcher/lang'
 import * as Warning from './components/Warning/lang'
-import * as LoginPage from './page/lang'
 
 export const en = {
-  LoginPage: LoginPage.en,
+  LoginPage: LoginForm.en,
   LoginWithLedger: LoginWithLedger.en,
   LoginWithTrezor: LoginWithTrezor.en,
   LoginMetamask: LoginMetamask.en,
@@ -37,7 +37,7 @@ export const en = {
 }
 
 export const ru = {
-  LoginPage: LoginPage.ru,
+  LoginPage: LoginForm.ru,
   LoginWithLedger: LoginWithLedger.ru,
   LoginWithTrezor: LoginWithTrezor.ru,
   LoginMetamask: LoginMetamask.ru,
