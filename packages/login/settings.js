39d0ab02c599706d07abde5f1e338bef348a3489
Andrey Antropov <q.laiff@gmail.com>
2017-12-08 13:43:49 +0500

    Correct include pattern

--- packages/login/settings.js
+++ packages/login/settings.js
@@ -1,19 +1,5 @@
-import MnemonicGenerateIcon from 'assets/img/mnemonic-key-color.svg'
 import { SESSION_CREATE, SESSION_DESTROY } from 'redux/session/actions'
-import theme from 'styles/themes/default'
-import inverted from 'styles/themes/inversed'
-import colors from 'styles/themes/variables'
 import web3Converter from 'utils/Web3Converter'
-
-export const assets = {
-  MnemonicGenerateIcon,
-}
-
-export const styles = {
-  colors,
-  inverted,
-  theme,
-}
 
 export const utils = {
   web3Converter,
