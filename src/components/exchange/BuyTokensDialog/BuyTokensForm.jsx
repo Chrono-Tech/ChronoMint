491f17ee349fe8f659c1e42f48e831ac43082ac4
AIS <abdulov001@gmail.com>
2017-12-11 13:51:59 +0300

    changes in exchange order book

---
+++ src/components/exchange/BuyTokensDialog/BuyTokensForm.jsx
@@ -0,0 +1,225 @@
+import globalStyles from 'layouts/partials/styles'
+import iconTokenDefaultSVG from 'assets/img/avaToken.svg'
+import { DUCK_MAIN_WALLET } from 'redux/mainWallet/actions'
+import BigNumber from 'bignumber.js'
+import { IPFSImage } from 'components'
+import TokenValue from 'components/common/TokenValue/TokenValue'
+import Immutable from 'immutable'
+import { RaisedButton } from 'material-ui'
+import ExchangeOrderModel from 'models/exchange/ExchangeOrderModel'
+import TokensCollection from 'models/exchange/TokensCollection'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { connect } from 'react-redux'
+import { Translate } from 'react-redux-i18n'
+import { TextField } from 'redux-form-material-ui'
+import { change, Field, formPropTypes, formValueSelector, isInvalid, reduxForm } from 'redux-form/immutable'
+import { approveTokensForExchange, DUCK_EXCHANGE, getTokensAllowance } from 'redux/exchange/actions'
+import './BuyTokensDialog.scss'
+import validate from './validate'
+
+function prefix (token) {
+  return `components.exchange.BuyTokensDialog.${token}`
+}
+
+export const FORM_EXCHANGE_BUY_TOKENS = 'ExchangeTokensForm'
+
+function mapStateToProps (state) {
+  const exchangeState = state.get(DUCK_EXCHANGE)
+  const selector = formValueSelector(FORM_EXCHANGE_BUY_TOKENS)
+  const invalidSelector = isInvalid(FORM_EXCHANGE_BUY_TOKENS)
+  return {
+    tokens: exchangeState.tokens(),
+    usersTokens: state.get(DUCK_MAIN_WALLET).tokens(),
+    buy: selector(state, 'buy'),
+    sell: selector(state, 'sell'),
+    formInvalid: invalidSelector(state),
+  }
+}
+
+const onSubmit = (values) => {
+  return new BigNumber(values.get('buy'))
+}
+
+@connect(mapStateToProps)
+@reduxForm({ form: FORM_EXCHANGE_BUY_TOKENS, validate, onSubmit })
+export default class BuyTokensForm extends React.PureComponent {
+  static propTypes = {
+    exchange: PropTypes.instanceOf(ExchangeOrderModel),
+    filter: PropTypes.instanceOf(Immutable.Map),
+    isBuy: PropTypes.bool,
+    tokens: PropTypes.instanceOf(TokensCollection),
+    usersTokens: PropTypes.instanceOf(Immutable.Map),
+    dispatch: PropTypes.func,
+    ...formPropTypes,
+  }
+
+  componentDidMount () {
+    !this.props.isBuy && this.props.dispatch(getTokensAllowance(this.props.exchange))
+  }
+
+  handleSetPrice = (e) => {
+    let value = e.target.value
+    const price = this.props.isBuy ? this.props.exchange.sellPrice() : this.props.exchange.buyPrice()
+    if (e.target.name === 'buy') {
+      this.props.dispatch(change(FORM_EXCHANGE_BUY_TOKENS, 'sell', new BigNumber(parseFloat(value) || 0).mul(price)))
+    } else {
+      this.props.dispatch(change(FORM_EXCHANGE_BUY_TOKENS, 'buy', new BigNumber(parseFloat(value) || 0).div(price)))
+    }
+  }
+
+  handleApprove = () => {
+    const allowance = this.props.usersTokens.get(this.props.exchange.symbol())
+      ? this.props.usersTokens.get(this.props.exchange.symbol()).allowance(this.props.exchange.address())
+      : 0
+    const token = this.props.usersTokens.get(this.props.exchange.symbol())
+
+    if (allowance > 0) {
+      this.props.dispatch(approveTokensForExchange(this.props.exchange, token, new BigNumber(0)))
+    } else {
+      this.props.dispatch(approveTokensForExchange(this.props.exchange, token, this.props.buy))
+    }
+  }
+
+  render () {
+    const exchangeToken = this.props.tokens.getBySymbol(this.props.exchange.symbol())
+    const ethToken = this.props.usersTokens.get('ETH')
+
+    const allowance = this.props.usersTokens.get(this.props.exchange.symbol())
+      ? this.props.usersTokens.get(this.props.exchange.symbol()).allowance(this.props.exchange.address())
+      : 0
+    return (
+      <form styleName='content' onSubmit={this.props.handleSubmit}>
+        <div styleName='row'>
+          <div styleName='leftCol'>
+            <div styleName='property'>
+              <div styleName='label'><Translate value={prefix('traderAddress')} />:</div>
+              <div styleName='value'>
+                <span>{this.props.exchange.address()}</span>
+              </div>
+            </div>
+            <div styleName='property'>
+              <div styleName='label'><Translate value={prefix('price')} />:</div>
+              <div>
+                <TokenValue
+                  value={this.props.isBuy ? this.props.exchange.sellPrice() : this.props.exchange.buyPrice()}
+                  symbol={ethToken.symbol()}
+                />
+              </div>
+            </div>
+            <div styleName='property'>
+              <div styleName='label'><Translate value={prefix('tradeLimits')} />:</div>
+              <div>
+                <TokenValue
+                  value={this.props.isBuy ? this.props.exchange.assetBalance() : this.props.exchange.ethBalance()}
+                  symbol={this.props.isBuy ? exchangeToken.symbol() : ethToken.symbol()}
+                />
+              </div>
+            </div>
+            {
+              !this.props.isBuy && allowance > 0 &&
+              <div styleName='property'>
+                <div styleName='label'><Translate value={prefix('allowance')} />:</div>
+                <div>
+                  <TokenValue
+                    value={allowance}
+                    symbol={exchangeToken.symbol()}
+                  />
+                </div>
+              </div>
+            }
+          </div>
+          <div styleName='rightCol'>
+            <div className='ByTokensDialog__form'>
+              <div className='row' styleName='amountsWrap'>
+                <div className='col-xs-2 col-sm-1' styleName='mobileFlex'>
+                  <div styleName='input'>
+                    <Field
+                      component={TextField}
+                      name='buy'
+                      fullWidth
+                      floatingLabelText={(
+                        <span><Translate value={prefix('amountIn')} />&nbsp;{exchangeToken.symbol()}</span>)}
+                      onChange={this.handleSetPrice}
+                    />
+                  </div>
+                  <div styleName='iconMobile' style={{ background: `#05326a` }}>
+                    <IPFSImage
+                      multihash={exchangeToken.icon()}
+                      fallback={iconTokenDefaultSVG}
+                      styleName='iconTitle'
+                    />
+                    <div styleName='tokenTitleMobile'>{exchangeToken.symbol()}</div>
+                  </div>
+                </div>
+                <div className='col-xs-2 col-sm-1' styleName='mobileFlex'>
+                  <div styleName='input'>
+                    <Field
+                      component={TextField}
+                      name='sell'
+                      fullWidth
+                      floatingLabelText={(
+                        <span><Translate value={prefix('amountIn')} />&nbsp;{ethToken.symbol()}</span>)}
+                      onChange={this.handleSetPrice}
+                    />
+                  </div>
+                  <div styleName='iconMobile' style={{ background: `#05326a` }}>
+                    <IPFSImage
+                      multihash={ethToken.icon()}
+                      fallback={iconTokenDefaultSVG}
+                      styleName='iconTitle'
+                    />
+                    <div styleName='tokenTitleMobile'>{ethToken.symbol()}</div>
+                  </div>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-xs-2'>
+                  <div styleName='actions'>
+                    {!this.props.isBuy &&
+                    <div>
+                      {allowance > 0
+                        ? <RaisedButton
+                          type='button'
+                          label={(
+                            <span styleName='buttonLabel'>
+                              <Translate value={prefix('revoke')} />
+                            </span>
+                          )}
+                          primary
+                          onTouchTap={this.handleApprove}
+                          {...globalStyles.buttonRaisedMultyLine}
+                        />
+                        : <RaisedButton
+                          disabled={this.props.pristine || !this.props.valid}
+                          type='button'
+                          label={(
+                            <span styleName='buttonLabel'>
+                              <Translate value={prefix('approve')} />
+                            </span>
+                          )}
+                          primary
+                          onTouchTap={this.handleApprove}
+                          {...globalStyles.buttonRaisedMultyLine}
+                        />
+                      }
+                    </div>
+                    }
+                    <RaisedButton
+                      disabled={!this.props.valid || (!this.props.isBuy && allowance.toString() !== this.props.buy)}
+                      type='submit'
+                      label={<span styleName='buttonLabel'><Translate value={prefix('sendRequest')} /></span>}
+                      {...globalStyles.buttonRaisedMultyLine}
+                      primary
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    )
+  }
+}
+
