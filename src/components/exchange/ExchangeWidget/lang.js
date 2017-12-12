07c535eda1af2ce633717c273bbbd902d6d02a2f
AIS <abdulov001@gmail.com>
2017-12-12 16:11:31 +0300

    Add preloader in exchanges filter

--- src/components/exchange/ExchangeWidget/lang.js
+++ src/components/exchange/ExchangeWidget/lang.js
@@ -8,7 +8,7 @@ export default {
     sell: 'Sell',
     amount: 'Amount',
     createExchange: 'Create an exchange',
-    middlewareDisconnected: 'The search is not available because MiddleWare is disconnected',
+    middlewareDisconnected: 'The search is not available because Middleware is disconnected',
   },
   ru: {
     exchange: 'Обмен',
@@ -19,6 +19,6 @@ export default {
     sell: 'Продать',
     amount: 'Сумма',
     createExchange: 'Создать обменник',
-    middlewareDisconnected: 'Поиск недоступен, так как MiddleWare отключен',
+    middlewareDisconnected: 'Поиск недоступен, так как Middleware отключен',
   },
 }
