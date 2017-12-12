147de01677a3761d328eb7e2043afa852243272f
dkchv <anton@dkchv.ru>
2017-12-12 15:14:47 +0300

    Update NetworkSelector.js

--- packages/login-ui/components/NetworkSelector/NetworkSelector.js
+++ packages/login-ui/components/NetworkSelector/NetworkSelector.js
@@ -52,7 +52,6 @@ export default class NetworkSelector extends PureComponent {
 
   resolveNetwork = () => {
     const web3 = new Web3()
-    web3Provider.reset()
     web3Provider.setWeb3(web3)
     web3Provider.setProvider(web3Utils.createStatusEngine(this.props.getProviderURL()))
     web3Provider.resolve()
