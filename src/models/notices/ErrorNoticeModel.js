9fb3eeda9e3e7eaf38cffbf4f00a8af151a4ca07
dkchv <anton@dkchv.ru>
2017-12-27 16:34:04 +0300

    add common error notify (#448)

---
+++ src/models/notices/ErrorNoticeModel.js
@@ -0,0 +1,23 @@
+import { I18n } from 'platform/i18n'
+import { Icons } from 'platform/icons'
+import { abstractNoticeModel } from './AbstractNoticeModel'
+
+export default class ErrorNoticeModel extends abstractNoticeModel({
+  message: null,
+}) {
+  icon () {
+    return Icons.get('notices.error.icon')
+  }
+
+  title () {
+    return I18n.t('notices.error.title')
+  }
+
+  details () {
+    return []
+  }
+
+  message () {
+    return this.get('message')
+  }
+}
