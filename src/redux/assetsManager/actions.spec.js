baea2208bf713f54ec0d514560f063656c5e7669
AIS <abdulov001@gmail.com>
2018-01-04 16:29:32 +0300

    Repair tests for assets manager

--- src/redux/assetsManager/actions.spec.js
+++ src/redux/assetsManager/actions.spec.js
@@ -1,16 +1,25 @@
-import BigNumber from 'bignumber.js'
 import contractsManagerDAO from 'dao/ContractsManagerDAO'
 import Immutable from 'immutable'
 import networkService from '@chronobank/login/network/NetworkService'
 import TokenModel from 'models/tokens/TokenModel'
 import { DUCK_SESSION } from 'redux/session/actions'
 import { accounts, mockStore } from 'specsInit'
+import FeeModel from 'models/tokens/FeeModel'
+import Amount from 'models/Amount'
+import ReissuableModel from 'models/tokens/ReissuableModel'
+import OwnerCollection from 'models/wallet/OwnerCollection'
+import { DUCK_TOKENS, TOKENS_FETCHED } from 'redux/tokens/actions'
+import TokensCollection from 'models/tokens/TokensCollection'
+import OwnerModel from 'models/wallet/OwnerModel'
 import * as a from './actions'
 
 let store
 const mock = new Immutable.Map({
   [ DUCK_SESSION ]: {
     account: accounts[ 0 ],
+  },
+  [ a.DUCK_ASSETS_MANAGER ]: {
+    assets: {},
   },
 })
 
@@ -32,6 +41,7 @@ describe('AssetsManager tests', () => {
   let addManagerTx = null
 
   it('should create platform', async (done) => {
+    store.clearActions()
     const watchCallback = async () => {
       await store.dispatch(a.getUsersPlatforms())
       const actions = store.getActions()
@@ -53,6 +63,7 @@ describe('AssetsManager tests', () => {
 
     const watchCallback = async (tx) => {
       expect(tx.args.token).toBeDefined()
+      createdToken = createdToken.set('address', tx.args.token)
       createdTokenTx = tx
       done()
     }
@@ -60,19 +71,23 @@ describe('AssetsManager tests', () => {
     const platformTokenExtensionGatewayManagerEmitterDAO = await contractsManagerDAO.getPlatformTokenExtensionGatewayManagerEmitterDAO()
     await platformTokenExtensionGatewayManagerEmitterDAO.watchAssetCreate(watchCallback, accounts[ 0 ])
 
-    store.dispatch(a.createAsset(new TokenModel({
+    createdToken = new TokenModel({
       decimals: 3,
       name: 'QQQ',
       symbol: 'QQQ',
-      balance: 10000,
+      balance: new Amount(10000, 'QQQ', true),
       icon: '',
-      fee: 1,
-      feeAddress: platform.address,
-      withFee: true,
+      fee: new FeeModel({
+        fee: 1,
+        withFee: true,
+        feeAddress: platform.address,
+      }),
       platform: platform,
-      totalSupply: 10000,
-      isReissuable: true,
-    })))
+      totalSupply: new Amount(10000, 'QQQ', true),
+      isReissuable: new ReissuableModel({ value: true }),
+    })
+
+    store.dispatch(a.createAsset(createdToken))
   })
 
   it('should get users Platforms', async (done) => {
@@ -104,27 +119,10 @@ describe('AssetsManager tests', () => {
     done()
   })
 
-  it('should get Tokens', async (done) => {
-    // await store.dispatch(a.getTokens())
-    const actions = store.getActions()
-    const action = actions[ actions.length - 1 ]
-    expect(action.type).toEqual(a.GET_TOKENS)
-    expect(action.payload.tokensMap.size).toEqual(2)
-    expect(Object.keys(action.payload.assets).length).toEqual(2)
-    // set created token
-    createdToken = action.payload.tokensMap.get('QQQ')
-    done()
-  })
-
   it('should get managers for asset symbol', async (done) => {
-    await store.dispatch(a.getManagersForAssetSymbol(createdToken.symbol()))
-    const actions = store.getActions()
-    const preLastAction = actions[ actions.length - 2 ]
-    const lastAction = actions[ actions.length - 1 ]
-    expect(preLastAction.type).toEqual(a.GET_MANAGERS_FOR_TOKEN_LOADING)
-    expect(lastAction.type).toEqual(a.GET_MANAGERS_FOR_TOKEN)
-    expect(lastAction.payload.symbol).toEqual('QQQ')
-    expect(lastAction.payload.managersForAssetSymbol.length).toEqual(1)
+    const result: OwnerCollection = await a.getManagersForAssetSymbol(createdToken.symbol())
+    createdToken = createdToken.managersList(result)
+    expect(result.size()).toEqual(1)
     done()
   })
 
@@ -169,36 +167,8 @@ describe('AssetsManager tests', () => {
   })
 
   it('should check is reissuable token', async (done) => {
-    await store.dispatch(a.isReissuable(createdToken))
-    const actions = store.getActions()
-    const lastAction = actions[ actions.length - 1 ]
-    expect(lastAction.type).toEqual(a.SET_IS_REISSUABLE)
-    expect(lastAction.payload.symbol).toEqual('QQQ')
-    done()
-  })
-
-  it('should set total supply', async (done) => {
-    const testsStore = mockStore(new Immutable.Map({
-      [ DUCK_SESSION ]: {
-        account: accounts[ 0 ],
-      },
-      [ a.DUCK_ASSETS_MANAGER ]: {
-        tokensMap: new Immutable.Map({
-          'QQQ': createdToken,
-        }),
-      },
-    }))
-
-    const tokenTotalSupply = createdToken.totalSupply()
-    await testsStore.dispatch(a.setTotalSupply(createdToken, new BigNumber(10), true))
-    await testsStore.dispatch(a.setTotalSupply(createdToken, new BigNumber(10), false))
-    const actions = testsStore.getActions()
-    const preLastAction = actions[ actions.length - 2 ]
-    const lastAction = actions[ actions.length - 1 ]
-    expect(preLastAction.type).toEqual(a.SET_TOTAL_SUPPLY)
-    expect(lastAction.type).toEqual(a.SET_TOTAL_SUPPLY)
-    // expect(preLastAction.payload.token.totalSupply()).toEqual(tokenTotalSupply.plus(createdToken.dao().removeDecimals(10)))
-    // expect(lastAction.payload.token.totalSupply()).toEqual(tokenTotalSupply.minus(createdToken.dao().removeDecimals(10)))
+    const result: ReissuableModel = await a.checkIsReissuable(createdToken, { platform: createdToken.platform().address })
+    expect(result).toBeTruthy()
     done()
   })
 
@@ -229,30 +199,25 @@ describe('AssetsManager tests', () => {
       },
       [ a.DUCK_ASSETS_MANAGER ]: {
         selectedToken: '',
-        tokensMap: new Immutable.Map({
-          'QQQ': createdToken,
-        }),
       },
+      [ DUCK_TOKENS ]: new TokensCollection().add(createdToken),
     }))
 
     await testsStore.dispatch(a.setManagers(addManagerTx))
     const newActions = testsStore.getActions()
     const lastAction = newActions[ newActions.length - 1 ]
-    expect(lastAction.type).toEqual(a.SET_NEW_MANAGERS_LIST)
-    expect(lastAction.payload.managers.length).toEqual(2)
-    expect(lastAction.payload.symbol).toEqual('QQQ')
-    expect(lastAction.payload.managersList.length).toEqual(1)
+    expect(lastAction.type).toEqual(TOKENS_FETCHED)
+    expect(lastAction.token.managersList()).toEqual(createdToken.managersList().add(new OwnerModel({ address: addManagerTx.args.to })))
     done()
   })
 
   it('should get fee', async (done) => {
-    await store.dispatch(a.getFee(createdToken))
-    const newActions = store.getActions()
-    const lastAction = newActions[ newActions.length - 1 ]
-    expect(lastAction.type).toEqual(a.SET_FEE)
-    expect(lastAction.payload.symbol).toEqual(createdToken.symbol())
-    expect(lastAction.payload.fee).toEqual(1)
-    expect(lastAction.payload.withFee).toEqual(true)
+    const result: FeeModel = await a.getFee(createdToken)
+    expect(result).toEqual(new FeeModel({
+      fee: 1,
+      withFee: true,
+      feeAddress: createdToken.platform().address,
+    }).isFetched(true))
     done()
   })
 
@@ -266,6 +231,47 @@ describe('AssetsManager tests', () => {
     const actions = testsStore.getActions()
     expect(actions[ 0 ].type).toEqual(a.GET_ASSETS_MANAGER_COUNTS_START)
     expect(result.length).toEqual(4)
+    done()
+  })
+
+  it('check select token', async (done) => {
+    const testsStore = mockStore(new Immutable.Map({
+      [ DUCK_SESSION ]: {
+        account: accounts[ 0 ],
+      },
+      [ a.DUCK_ASSETS_MANAGER ]: {
+        selectedToken: '',
+        assets: {
+          [ createdToken.address() ]: {
+            platform: createdToken.platform().address,
+          },
+        },
+      },
+      [ DUCK_TOKENS ]: new TokensCollection().add(createdToken),
+    }))
+    await testsStore.dispatch(a.selectToken(createdToken))
+    const actions = testsStore.getActions()
+    expect(actions[ 0 ].type).toEqual(a.SELECT_TOKEN)
+    expect(actions[ 0 ].payload).toEqual({ symbol: 'QQQ' })
+
+    expect(actions[ 1 ].type).toEqual(TOKENS_FETCHED)
+    expect(actions[ 1 ].token).toEqual(createdToken
+      .managersList(new OwnerCollection().isFetching(true))
+      .fee(createdToken.fee().isFetching(true))
+      .isReissuable(createdToken.isReissuable().isFetching(true)))
+
+    expect(actions[ 2 ].type).toEqual(TOKENS_FETCHED)
+    expect(actions[ 2 ].token).toEqual(createdToken
+      .managersList(createdToken.managersList())
+      .fee(new FeeModel({
+        fee: 1,
+        withFee: true,
+        feeAddress: createdToken.platform().address,
+      }).isFetched(true))
+      .isReissuable(
+        new ReissuableModel({ value: true })
+          .isFetched(true)))
+
     done()
   })
 })
