6778481cd6213b36c1d186d9100c27f7eb456a2b
AIS <abdulov001@gmail.com>
2018-02-01 17:07:31 +0300

    In first time to show only the address field. After entering the address, receive a token data. Then enter the received data into the form.
    (cherry picked from commit 704248b)

--- src/components/dialogs/CBETokenDialog/CBETokenDialog.jsx
+++ src/components/dialogs/CBETokenDialog/CBETokenDialog.jsx
@@ -1,15 +1,17 @@
-import { Field, formPropTypes, reduxForm } from 'redux-form/immutable'
-import { FlatButton, RaisedButton } from 'material-ui'
+import { Field, formPropTypes, formValueSelector, reduxForm } from 'redux-form/immutable'
 import { I18n } from 'platform/i18n/index'
+import { FlatButton, RaisedButton } from 'material-ui'
+import { Translate } from 'react-redux-i18n'
 import PropTypes from 'prop-types'
 import React, { PureComponent } from 'react'
 import { TextField } from 'redux-form-material-ui'
 import { connect } from 'react-redux'
 import { ACCEPT_IMAGES } from 'models/FileSelect/FileExtension'
-import { addToken, formTokenLoadMetaData, modifyToken } from 'redux/settings/erc20/tokens/actions'
+import { addToken, formTokenLoadMetaData, getDataFromContract, modifyToken } from 'redux/settings/erc20/tokens/actions'
 import { modalsClose } from 'redux/modals/actions'
 import FileSelect from 'components/common/FileSelect/FileSelect'
 import ModalDialog from 'components/dialogs/ModalDialog'
+import TokenModel from 'models/tokens/TokenModel'
 import validate from './validate'
 
 import '../FormDialog.scss'
@@ -17,14 +19,19 @@ import '../FormDialog.scss'
 export const FORM_CBE_TOKEN = 'CBETokenDialog'
 
 function mapStateToProps (state) {
+  const selector = formValueSelector(FORM_CBE_TOKEN)
   return {
     isFetching: state.get('settingsERC20Tokens').formFetching,
+    address: selector(state, 'address'),
+    symbolFromContract: selector(state, 'symbolFromContract'),
+    decimalsFromContract: selector(state, 'decimalsFromContract'),
   }
 }
 
 function mapDispatchToProps (dispatch, ownProps) {
   return {
     onClose: () => dispatch(modalsClose()),
+    getDataFromContract: (e, address) => dispatch(getDataFromContract(new TokenModel({ address }))),
     onSubmit: (values) => {
       dispatch(modalsClose())
       if (ownProps.isModify) {
@@ -41,7 +48,7 @@ function mapDispatchToProps (dispatch, ownProps) {
   form: FORM_CBE_TOKEN,
   validate,
   asyncValidate: (token, dispatch) => formTokenLoadMetaData(token, dispatch, FORM_CBE_TOKEN),
-  asyncBlurFields: [ 'address', 'symbol' ],
+  asyncBlurFields: [ 'address', 'symbol', 'decimals' ],
 })
 export default class CBETokenDialog extends PureComponent {
   static propTypes = {
@@ -49,6 +56,8 @@ export default class CBETokenDialog extends PureComponent {
     isFetching: PropTypes.bool,
     handleAddressChange: PropTypes.func,
     onClose: PropTypes.func,
+    address: PropTypes.string,
+    getDataFromContract: PropTypes.func,
     ...formPropTypes,
   }
 
@@ -62,7 +71,7 @@ export default class CBETokenDialog extends PureComponent {
             <h3
               styleName='title'
             >
-              {I18n.t(this.props.isModify ? 'settings.erc20.tokens.modify' : 'settings.erc20.tokens.add')}
+              {<Translate value={this.props.isModify ? 'settings.erc20.tokens.modify' : 'settings.erc20.tokens.add'} />}
             </h3>
           </div>
           <div styleName='content'>
@@ -71,49 +80,56 @@ export default class CBETokenDialog extends PureComponent {
               name='address'
               fullWidth
               disabled={this.props.isFetching}
-              floatingLabelText={I18n.t('common.ethAddress')}
+              floatingLabelText={<Translate value={'common.ethAddress'} />}
+              onBlur={this.props.getDataFromContract}
             />
-            <Field
-              component={TextField}
-              name='name'
-              fullWidth
-              disabled={this.props.isFetching}
-              floatingLabelText={I18n.t('common.name')}
-            />
-            <Field
-              component={TextField}
-              name='symbol'
-              fullWidth
-              disabled={this.props.isFetching}
-              floatingLabelText={I18n.t('settings.erc20.tokens.symbol')}
-            />
-            <Field
-              component={TextField}
-              name='decimals'
-              fullWidth
-              disabled={this.props.isFetching}
-              floatingLabelText={I18n.t('settings.erc20.tokens.decimals')}
-            />
-            <Field
-              component={TextField}
-              name='url'
-              fullWidth
-              floatingLabelText={I18n.t('settings.erc20.tokens.url')}
-            />
-            <Field
-              component={FileSelect}
-              name='icon'
-              value={this.props.initialValues.icon()}
-              fullWidth
-              label={I18n.t('wallet.selectTokenIcon')}
-              accept={ACCEPT_IMAGES}
-            />
+            {
+              this.props.address &&
+              <div>
+                <Field
+                  component={TextField}
+                  name='name'
+                  fullWidth
+                  disabled={this.props.isFetching}
+                  floatingLabelText={<Translate value={'common.name'} />}
+                />
+                <Field
+                  component={TextField}
+                  name='symbol'
+                  fullWidth
+                  disabled={this.props.isFetching}
+                  floatingLabelText={<Translate value={'settings.erc20.tokens.symbol'} />}
+                />
+                <Field
+                  component={TextField}
+                  name='decimals'
+                  fullWidth
+                  disabled={this.props.isFetching}
+                  floatingLabelText={<Translate value={'settings.erc20.tokens.decimals'} />}
+                />
+                <Field
+                  component={TextField}
+                  name='url'
+                  fullWidth
+                  floatingLabelText={<Translate value={'settings.erc20.tokens.url'} />}
+                />
+                <Field
+                  component={FileSelect}
+                  name='icon'
+                  value={this.props.initialValues.icon()}
+                  fullWidth
+                  label={I18n.t('wallet.selectTokenIcon')}
+                  accept={ACCEPT_IMAGES}
+                />
+              </div>
+            }
           </div>
           <div styleName='footer'>
             <FlatButton styleName='action' label='Cancel' onTouchTap={() => this.props.onClose()} />
             <RaisedButton
               styleName='action'
-              label={I18n.t(this.props.isModify ? 'settings.erc20.tokens.modify' : 'settings.erc20.tokens.add')}
+              label={<Translate
+                value={this.props.isModify ? 'settings.erc20.tokens.modify' : 'settings.erc20.tokens.add'} />}
               primary
               type='submit'
             />
