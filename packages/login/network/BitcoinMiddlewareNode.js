33204f093779ad4f9c2ba45a452e48ef98042381
AIS <abdulov001@gmail.com>
2018-02-21 13:49:18 +0300

    Start implement Middleware integration in Bitcoin
    (cherry picked from commit 2529d94)

--- packages/login/network/BitcoinMiddlewareNode.js
+++ packages/login/network/BitcoinMiddlewareNode.js
@@ -1,6 +1,11 @@
+import axios from 'axios'
+import networkService from '@chronobank/login/network/NetworkService'
 import BigNumber from 'bignumber.js'
 import BitcoinAbstractNode, { BitcoinTx, BitcoinBalance } from './BitcoinAbstractNode'
 import { DECIMALS } from './BitcoinEngine'
+import { NETWORK_MAIN_ID, MIDDLEWARE_MAP, LOCAL_ID } from './settings'
+import TxModel from '../../../src/models/TxModel'
+import { TXS_PER_PAGE } from '../../../src/models/wallet/TransactionsCollection'
 
 export default class BitcoinMiddlewareNode extends BitcoinAbstractNode {
   constructor ({ feeRate, ...args }) {
@@ -84,8 +89,79 @@ export default class BitcoinMiddlewareNode extends BitcoinAbstractNode {
     }
   }
 
-  async getTransactionsList (/*address*/) {
+  async getTransactionsList (address, id, offset) {
+    const { network } = networkService.getProviderSettings()
+    const links = MIDDLEWARE_MAP.txHistory[ `${id}`.toLowerCase() ]
+
+    let apiURL = ''
+    switch (network.id) {
+      case NETWORK_MAIN_ID:
+        apiURL = links.mainnet
+        break
+      case LOCAL_ID:
+        apiURL = links.local
+        break
+      default:
+        apiURL = links.testnet
+    }
+    if (apiURL) {
+      try {
+        const test = await axios.get(`${apiURL}/tx/${address}/history?skip=0&limit=1`)
+        if (test.status === 200) {
+          return this._getTransferFromMiddleware(apiURL, address, id, offset)
+        }
+      } catch (e) {
+        // eslint-disable-next-line
+        console.warn('Middleware API is not available, fallback to block-by-block scanning', e)
+      }
+    }
+
     return []
+  }
+
+  async _getTransferFromMiddleware (apiURL, account, id, offset): Array<TxModel> {
+    const cache = this._getFilterCache(id) || {}
+    const toBlock = cache.toBlock || await this._web3Provider.getBlockNumber()
+    const txs = cache.txs || []
+    let page = cache.page || 1
+    let end = cache.end || false
+
+    while (txs.length < TXS_PER_PAGE && !end) {
+      const url = `${apiURL}/tx/${account}/history?skip=0&limit=${offset}`
+      // eslint-disable-next-line
+      console.log('_getTransferFromMiddleware url', url)
+      try {
+        const result = await axios.get(url)
+        // eslint-disable-next-line
+        console.log('_getTransferFromMiddleware result', result)
+        if (typeof result !== 'object' || !result.data) {
+          throw new Error('invalid result')
+        }
+        if (result.status !== 200) {
+          throw new Error(`result not OK: ${result.data.message}`)
+        }
+        for (const tx of result.data) {
+          if (!tx.value || tx.value === '0') {
+            continue
+          }
+          txs.push(this._getTxModel(tx, account, tx.timeStamp))
+        }
+        if (txs.length === 0) {
+          end = true
+        }
+      } catch (e) {
+        end = true
+        // eslint-disable-next-line
+        console.warn('EthereumDAO getTransfer Middleware', e)
+      }
+      page++
+    }
+
+    this._setFilterCache(id, {
+      toBlock, page, txs: txs.slice(TXS_PER_PAGE), end,
+    })
+
+    return txs.slice(0, TXS_PER_PAGE)
   }
 
   getFeeRate (): Promise {
