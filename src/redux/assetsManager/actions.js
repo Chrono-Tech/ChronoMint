aec3746403e81cfd17fca816fd782b49ab66fb30
maximus <kalashnikov.tema@gmail.com>
2018-03-16 17:10:34 +0300

    little fixes assetManager

--- src/redux/assetsManager/actions.js
+++ src/redux/assetsManager/actions.js
@@ -480,7 +480,6 @@ const subscribeToAssetEvents = (account: string) => async (dispatch) => {
   const assetsManagerDao = await contractManager.getAssetsManagerDAO()
 
   assetsManagerDao.subscribeOnMiddleware('platformrequested', (data) => {
-    console.log('platformrequested: ', data, account)
     if (data && data.payload && data.payload.by !== account) {
       return
     }
@@ -488,7 +487,6 @@ const subscribeToAssetEvents = (account: string) => async (dispatch) => {
   })
 
   assetsManagerDao.subscribeOnMiddleware('assetcreated', (data) => {
-    console.log('assetcreated: ', data, account)
     if (data && data.payload && data.payload.by !== account) {
       return
     }
