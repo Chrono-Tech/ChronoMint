b715388021038b033dc37dbfb23cd13ceb666277
AIS <abdulov001@gmail.com>
2018-04-13 18:00:56 +0300

    Added second step to deposit form.

--- src/components/common/TokenValue/TokenValue.js
+++ src/components/common/TokenValue/TokenValue.js
@@ -97,7 +97,7 @@ class TokenValue extends PureComponent {
       : (
         <span styleName={defaultMod} className='TokenValue__root' style={style}>
           {prefix}
-          <span styleName='integral' className='TokenValue__integral'>{symbol} {integerWithDelimiter(valueWithoutDecimals)}</span>
+          <span styleName='integral' className='TokenValue__integral'>{!noRenderSymbol && symbol} {integerWithDelimiter(valueWithoutDecimals)}</span>
           <span styleName='integral' className='TokenValue__fraction'>{this.getFraction(valueWithoutDecimals)}</span>
           {!noRenderPrice && this.renderPrice(valueWithoutDecimals, symbol)}
         </span>
