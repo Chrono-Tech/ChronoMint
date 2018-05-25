f20da71adcb358254e31e9ebcefd352ae36a009a
maximus <kalashnikov.tema@gmail.com>
2018-05-25 19:15:27 +0300

    login new layout

--- src/layouts/Splash/Splash.js
+++ src/layouts/Splash/Splash.js
@@ -7,6 +7,8 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import WalletTitleBG from 'assets/img/wallet-title-bg.png'
 import StripesToCrop from 'assets/img/stripes-2-crop.jpg'
+import ChronoWalletLogoBright from 'assets/img/chronowalletlogobright.svg'
+import ChronoWalletTextBright from 'assets/img/chronowallettextbright.svg'
 
 import Footer from '../Footer/Footer'
 
@@ -24,12 +26,21 @@ class Splash extends Component {
           <div styleName='header-picture'>
             <img src={StripesToCrop} />
           </div>
-          <div styleName='header-picture'>
+          <div styleName='header-picture-crop'>
             <img src={WalletTitleBG} />
+          </div>
+          <div styleName='header-logos'>
+            <img styleName='chrono-wallet-logo-bright' src={ChronoWalletLogoBright} />
+            <img styleName='chrono-wallet-text-bright' src={ChronoWalletTextBright} />
           </div>
         </div>
         <div styleName='content'>
-          {this.props.children}
+          <div styleName='create-title'>
+            {'Create New Account'}
+          </div>
+          <div styleName='create-title-description'>
+            {'Created wallet will be encrypted using given password and stored in your browser\'s local storage.'}
+          </div>
         </div>
         {!window.isMobile && (<Footer />)}
       </div>
--- src/pages/lib/WalletsPage.jsx
+++ src/pages/lib/WalletsPage.jsx
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2017–2018, LaborX PTY
+ * Licensed under the AGPL Version 3 license.
+ */
+
 import { CSSTransitionGroup } from 'react-transition-group'
 import Partials from 'layouts/partials'
 import React, { Component } from 'react'
