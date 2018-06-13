2ae9b7256b7251ce684950c160d798fb0909184c
Oleg Dizus <olegdizus@gmail.com>
2018-06-13 17:12:31 +0300

    Add platform-dependent module reexports
    Refers to: CM-58

---
+++ packages/core/redux/ui/actions.js
@@ -0,0 +1,8 @@
+/**
+ * Copyright 2017–2018, LaborX PTY
+ * Licensed under the AGPL Version 3 license.
+ */
+
+import * as actions from '@chronobank/core-dependencies/redux/ui/actions'
+
+export default actions
---
+++ packages/core/utils/i18n/index.js
@@ -0,0 +1,6 @@
+/**
+ * Copyright 2017–2018, LaborX PTY
+ * Licensed under the AGPL Version 3 license.
+ */
+
+export * from '@chronobank/core-dependencies/i18n'
