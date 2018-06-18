83e8a1bc82879bb774d573b8bfa90a2b52685d36
AIS <abdulov001@gmail.com>
2018-06-18 12:03:30 +0300

    Implement new design for live notifications.

--- src/layouts/Markup.jsx
+++ src/layouts/Markup.jsx
@@ -19,6 +19,7 @@ import { DUCK_SIDES, SIDES_TOGGLE_MAIN_MENU } from 'redux/sides/actions'
 import { DUCK_MODALS } from 'redux/modals/actions'
 import './Markup.scss'
 import { DrawerMainMenu, HeaderPartial } from './partials'
+import TransferNoticeModel from '../models/notices/TransferNoticeModel'
 
 function mapStateToProps (state) {
   return {
@@ -88,7 +89,7 @@ export default class Markup extends PureComponent {
     if (!currentPage) {
       Object.keys(BUTTONS).map((path) => {
         if (path === pathname) {
-          currentPage = BUTTONS[ path ]
+          currentPage = BUTTONS[path]
         }
       })
     }
