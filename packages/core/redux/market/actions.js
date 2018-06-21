0910d95ba79ef4faa71d11621dfac6d7930721b5
Oleg Dizus <olegdizus@gmail.com>
2018-06-21 14:01:50 +0300

    Fixup market actions names

--- packages/core/redux/market/actions.js
+++ packages/core/redux/market/actions.js
@@ -9,12 +9,12 @@ import axios from 'axios'
 
 export const DUCK_MARKET = 'market'
 
-export const MARKET_INIT = '../../market/INIT'
-export const MARKET_ADD_TOKEN = '../../market/ADD_TOKEN'
-export const MARKET_UPDATE_PRICES = '../../market/UPDATE_PRICES'
-export const MARKET_UPDATE_RATES = '../../market/UPDATE_RATES'
-export const LAST_MARKET_UPDATE = '../../market/UPDATE_LAST_MARKET'
-export const SET_SELECTED_COIN = '../../market/SET_SELECTED_COIN'
+export const MARKET_INIT = 'market/INIT'
+export const MARKET_ADD_TOKEN = 'market/ADD_TOKEN'
+export const MARKET_UPDATE_PRICES = 'market/UPDATE_PRICES'
+export const MARKET_UPDATE_RATES = 'market/UPDATE_RATES'
+export const LAST_MARKET_UPDATE = 'market/UPDATE_LAST_MARKET'
+export const SET_SELECTED_COIN = 'market/SET_SELECTED_COIN'
 
 const MARKET_REQUEST_DELAY = 30000
 export let timerId
