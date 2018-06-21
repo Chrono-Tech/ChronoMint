184f1d8d8c49e87e32fa96ffc5e6ada8ece0cbd4
Oleg Dizus <olegdizus@gmail.com>
2018-06-21 14:32:32 +0300

    Fix code style

--- src/redux/ducks.js
+++ src/redux/ducks.js
@@ -4,26 +4,26 @@
  */
 
 import Login from '@chronobank/login/redux/ducks'
+import * as assetsHolder from '@chronobank/core/redux/assetsHolder'
 import * as assetsManager from '@chronobank/core/redux/assetsManager'
-import * as drawer from './drawer'
 import * as exchange from '@chronobank/core/redux/exchange'
 import * as locs from '@chronobank/core/redux/locs'
 import * as mainWallet from '@chronobank/core/redux/mainWallet'
 import * as market from '@chronobank/core/redux/market'
-import * as modals from './modals'
-import * as sides from './sides'
 import * as multisigWallet from '@chronobank/core/redux/multisigWallet'
 import * as notifier from '@chronobank/core/redux/notifier'
 import * as operations from '@chronobank/core/redux/operations'
 import * as rewards from '@chronobank/core/redux/rewards'
 import * as session from '@chronobank/core/redux/session'
 import * as settings from '@chronobank/core/redux/settings'
-import * as ui from './ui'
+import * as tokens from '@chronobank/core/redux/tokens'
 import * as voting from '@chronobank/core/redux/voting'
 import * as wallet from '@chronobank/core/redux/wallet'
 import * as watcher from '@chronobank/core/redux/watcher'
-import * as tokens from '@chronobank/core/redux/tokens'
-import * as assetsHolder from '@chronobank/core/redux/assetsHolder'
+import * as drawer from './drawer'
+import * as modals from './modals'
+import * as sides from './sides'
+import * as ui from './ui'
 
 export default {
   ui,
