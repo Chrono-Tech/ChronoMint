70258566859fa8a049467391113d8ef18486eaea
Alexey Ozerov <alexozerov@gmail.com>
2018-07-06 12:02:28 +0300

    Moved src/platform into packages/@chronobank/core-depndencies
    Fixed imports

--- src/components/common/TokenValue/TokenValue.js
+++ src/components/common/TokenValue/TokenValue.js
@@ -13,7 +13,7 @@ import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { DUCK_MARKET } from '@chronobank/core/redux/market/actions'
 import { DUCK_TOKENS } from '@chronobank/core/redux/tokens/actions'
-import { integerWithDelimiter } from 'platform/utils/formatter'
+import { integerWithDelimiter } from '@chronobank/core-dependencies/utils/formatter'
 
 import './TokenValue.scss'
 
