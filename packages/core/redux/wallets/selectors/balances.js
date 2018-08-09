69138696d042e0abfa21209e04e12cde04e2800e
maximus <kalashnikov.tema@gmail.com>
2018-08-09 12:17:05 +0300

    MINT-1719 eslint fixes

--- packages/core/redux/wallets/selectors/balances.js
+++ packages/core/redux/wallets/selectors/balances.js
@@ -88,12 +88,10 @@ const balanceCalculator = (walletId, symbol) => createSelector(
   ) => {
     return balances
       .reduce((accumulator, tokenBalance) => {
-        let accumulatorResult = null
         const symbol = Object.keys(tokenBalance)[0]
         const balance = Object.values(tokenBalance)[0]
         const tokenPrice = priceList[symbol] && priceList[symbol][selectedCurrency] || null
-        accumulatorResult = accumulator + ((balance || 0) * (tokenPrice || 0))
-        return accumulatorResult
+        return accumulator + ((balance || 0) * (tokenPrice || 0))
       }, 0)
   },
 )
