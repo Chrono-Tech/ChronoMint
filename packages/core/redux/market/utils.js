a19e5ef1e34edfda15653b6224a05193a070e05e
Alexey Ozerov <alexozerov@gmail.com>
2018-08-24 09:55:53 +0300

    CM-111: applied CR comments
    Improved readability
    Improved calculation algorithm

--- packages/core/redux/market/utils.js
+++ packages/core/redux/market/utils.js
@@ -6,24 +6,29 @@
 // eslint-disable-next-line import/prefer-default-export
 export const chunker = (tokens) => {
 
-  const chunk = (list, chunkSize = 5) => {
+  // chunkSize is 35 by default. Lenght of a token may be 3 or 6 characters (especially in testnet)
+  // 3*35 or 6*35 = ~200 or ~300 characters in comma separated string
+  const chunk = (list, requiredChunkSize = 35) => {
     if (!list || !list.length) {
       return []
     }
 
-    let i, j, t
-    const chunks = []
-    for (i = 0, j = list.length; i < j; i += chunkSize) {
-      t = list.slice(i, i + chunkSize)
-      chunks.push(t)
+    // Let's limit chunks size to 35 due to the reasons in comment above
+    const chunkSize = requiredChunkSize > 35 ? 35 : requiredChunkSize
+    let chunkStartIndex, allTokensLength, singleChunk
+    const chunks = [] // array of arrays (chunks)
+
+    for (chunkStartIndex = 0, allTokensLength = list.length; chunkStartIndex < allTokensLength; chunkStartIndex += chunkSize) {
+      singleChunk = list.slice(chunkStartIndex, chunkStartIndex + chunkSize)
+      chunks.push(singleChunk)
     }
 
     return chunks
   }
 
-  const tokenList = tokens.join(',')
-  const tLength = tokenList.length // length of comma-separated string of tokens
+  const tLength = tokens.join(',').length // length of comma-separated string of tokens
   const chunkCount = tLength > 600 ? 3 : 2 // amount of chunks required
-  const chunks = chunk(tokens, Math.ceil(tokens.length/chunkCount))
+  const requiredChunkSize =  Math.ceil(tokens.length/chunkCount)
+  const chunks = chunk(tokens, requiredChunkSize)
   return chunks
 }
