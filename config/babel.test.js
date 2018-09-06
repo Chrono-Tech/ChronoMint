b36b2a30dda61eb01bf1d07ddc65ca11d9c76721
Alex Elgin <sascha.elgin@gmail.com>
2018-09-06 21:18:08 +0600

    fix Jest setup (`yarn test` command works now)

---
+++ config/babel.test.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    [
+      'env',
+      {
+        modules: false
+      }
+    ],
+    'react'
+  ],
+  plugins: [
+    'transform-class-properties',
+    'transform-es2015-modules-commonjs'
+  ]
+};
