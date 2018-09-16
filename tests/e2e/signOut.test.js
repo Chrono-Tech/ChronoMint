a8d316f30f6f970541300e3e9e7dbc45835eafbd
Alex Elgin <sascha.elgin@gmail.com>
2018-09-16 16:31:02 +0600

    MINT-1808 add 'Sing out'e2e test

---
+++ tests/e2e/signOut.test.js
@@ -0,0 +1,24 @@
+/*global TimeoutLength*/
+import { openBrowser, openPage } from './utils';
+import { checkNotAuthorized, signInByMnemonicKey, signOut } from './utils/signInOutUp';
+
+describe('Sign Out', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await openBrowser();
+    page = await openPage(browser);
+  });
+
+  it('signs out successfully', async () => {
+    await signInByMnemonicKey(page, 'sign-in-out', 'sign-in-out',
+      'until track swap effort secret regret found forum monitor gown boy domain');
+    await signOut(page);
+    await checkNotAuthorized(page);
+  }, TimeoutLength);
+
+  afterAll(() => {
+    browser.close();
+  });
+});
