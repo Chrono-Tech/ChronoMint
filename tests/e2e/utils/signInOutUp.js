0fe8bd1aa8d5d9f1c6c98812027a42c678047d5c
Alexander Elgin <sascha.elgin@gmail.com>
2018-09-18 12:33:59 +0600

    MINT-1800 configure e2e testing env (#806)
    * MINT-1800 use for e2e tests Puppeteer (instead of Selenium)
    
    * MINT-1800 add sing in & sign out  utils
    
    * MINT-1800 simplify the e2e tests configuration
    
    * MINT-1800 add the jest-puppeteer config
    
    * MINT-1800 speed up the e2e tests

---
+++ tests/e2e/utils/signInOutUp.js
@@ -0,0 +1,45 @@
+/*global TimeoutLength*/
+import { clickByXpahSelector, clickText, typeFieldValue } from './';
+
+export const submitSignInForm = async function (page, username, password) {
+  await page.waitForXPath(`//*[text()="${username}"]`);
+  await clickText(page, username);
+  await page.waitForSelector('[name=password]');
+  await typeFieldValue(page, '[name=password]', password);
+  await clickByXpahSelector(page, '//*[contains(@class, "Button__login_")]');
+  await page.waitForXPath('//*[text()="Add a wallet"]');
+};
+
+export async function checkAuthorized (page) {
+  await expect(page).toMatch('Add a wallet');
+}
+
+export async function checkNotAuthorized (page) {
+  await expect(page).not.toMatch('Add a wallet');
+}
+
+export async function submitSignUpForm (page, username, password) {
+  await page.waitForSelector('[name=walletName]');
+  await typeFieldValue(page, '[name=walletName]', username);
+  await typeFieldValue(page, '[name=password]', password);
+  await typeFieldValue(page, '[name=confirmPassword]', password);
+  await clickText(page, 'Create new account');
+}
+
+export async function signInByMnemonicKey (page, username, password, mnemonicKey) {
+  await page.goto('https://localhost:3000/login/select-account', { timeout: TimeoutLength });
+  await clickText(page, 'Add an existing account');
+  await clickText(page, 'Mnemonic Key');
+
+  await typeFieldValue(page, '[name=mnemonic]', mnemonicKey);
+  await clickText(page, 'Submit');
+
+  await submitSignUpForm(page, username, password);
+  await page.waitForXPath('//*[text()="Finish"]');
+  await clickText(page, 'Finish');
+  await submitSignInForm(page, username, password);
+}
+
+export async function signOut (page) {
+  await clickByXpahSelector(page, '//*[@title="Logout"]');
+}
