dfeb8e68b80f042182f13951a74b9c5bd85f3076
Alexander Elgin <sascha.elgin@gmail.com>
2018-09-19 03:15:12 +0600

    MINT-1811, MINT-1812, MINT-1813, MINT-1814 send funds e2e tests (#813)
    * MINT-1811 add the 'Send ETH' test
    
    * MINT-1812 add the 'Send BTC' test
    
    * MINT-1813 add the 'Send NEM' test
    
    * MINT-1814 add the 'Send WAVES' test

---
+++ tests/e2e/utils/sendFunds.js
@@ -0,0 +1,69 @@
+import { clickText, typeFieldValue } from '.';
+import { openMyAccountsPage, signInByMnemonicKey, submitSignInForm, signOut } from './signInOutUp';
+
+async function getCurrencyMainJsHandle (page, address) {
+  return await page.evaluateHandle(addressNode => addressNode.parentNode.parentNode.parentNode,
+    (await page.$x(`//span[contains(@class,"WalletWidget__address-address_")][text()="${address}"]`))[0]);
+}
+
+async function getBalance (page, address, currencyName) {
+  const currencyMainJsHandle = await getCurrencyMainJsHandle(page, address);
+  const balance = await page.evaluate(node => node.querySelector('[class^=WalletWidget__crypto-amount_]').textContent,
+    currencyMainJsHandle);
+  return parseFloat(balance.replace(currencyName, '').trim());
+}
+
+async function sendFunds (page, senderAddress, recipientAddress, amount) {
+  const currencyMainJsHandle = await getCurrencyMainJsHandle (page, senderAddress);
+  await (await currencyMainJsHandle.asElement().$('button')).click();
+  await page.waitForSelector('[name=recipient]');
+  await typeFieldValue(page, '[name=recipient]', recipientAddress);
+  await typeFieldValue(page, '[name=amount]', amount.toString());
+  await clickText(page, 'Send');
+  await page.waitForXPath('//*[text()="Confirm"]');
+  await clickText(page, 'Confirm');
+}
+
+function checkBalanceChange (actualChange, amount, estimatedTransferFee) {
+  expect(actualChange >= amount - estimatedTransferFee).toBe(true);
+  expect(actualChange <= amount).toBe(true);
+}
+
+export default async function send (page, senderAddress, recipientAddress, currencyName, amount, estimatedTransferFee) {
+  const sender = {
+    address: senderAddress,
+    username: 'sender',
+    password: 'sender',
+    mnemonicKey: 'consider injury federal west guitar nut blast maple quick unhappy multiply night'
+  };
+
+  await signInByMnemonicKey(page, sender.username, sender.password, sender.mnemonicKey);
+  sender.initialBalance = await getBalance(page, sender.address, currencyName);
+  await signOut(page);
+
+  const recipient = {
+    address: recipientAddress,
+    username: 'recipient',
+    password: 'recipient',
+    mnemonicKey: 'prize transfer park kitten glass hungry sibling abstract saddle lend wait layer'
+  };
+
+  await signInByMnemonicKey(page, recipient.username, recipient.password, recipient.mnemonicKey);
+  recipient.initialBalance = await getBalance(page, recipient.address, currencyName);
+  await signOut(page);
+
+  await openMyAccountsPage(page);
+  await submitSignInForm(page, sender.username, sender.password);
+
+  await sendFunds(page, sender.address, recipient.address, amount);
+  sender.currentBalance = await getBalance(page, sender.address, currencyName);
+  await signOut(page);
+
+  await page.waitFor(16000);
+  await openMyAccountsPage(page);
+  await submitSignInForm(page, recipient.username, recipient.password);
+
+  recipient.currentBalance = await getBalance(page, recipient.address, currencyName);
+  checkBalanceChange(recipient.currentBalance - recipient.initialBalance, amount, estimatedTransferFee);
+  await sendFunds(page, recipient.address, sender.address, amount - estimatedTransferFee);
+}
