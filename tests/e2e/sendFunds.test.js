1183ef8cfeee0ea1a8af7513faf648be0c367cef
Alex Elgin <sascha.elgin@gmail.com>
2018-09-18 17:12:25 +0600

    MINT-1812 add the 'Send BTC' test

--- tests/e2e/sendFunds.test.js
+++ tests/e2e/sendFunds.test.js
@@ -12,6 +12,11 @@ describe('Send Funds', () => {
     page = await openPage(browser);
   });
 
+  it('sends BTC', async () => {
+    await sendFunds(page, 'mpMD85MdtkaBLYNkSMXZcpn3PEW15NNteG', 'mszetFaax2z9XfWNc3XgEVSSuqDRV51ALh',
+      'BTC', 0.02, 0.001);
+  }, TimeoutLength);
+
   it('sends ETH', async () => {
     await sendFunds(page, '0x44396dded98751d285321dc61836226caffe0dbd', '0xa7ec4a28f7c39650d7cce33ef9847bccee02e993',
       'ETH', 0.02, 0.0001);
