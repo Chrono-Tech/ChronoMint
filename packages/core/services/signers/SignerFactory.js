3ccd25a1e3a6db3c74d083a69c349a894ee4e566
Alex Elgin <sascha.elgin@gmail.com>
2018-09-27 13:16:41 +0600

    MINT-1881 refactor the SignerFactory

--- packages/core/services/signers/SignerFactory.js
+++ packages/core/services/signers/SignerFactory.js
@@ -51,6 +51,24 @@ import WavesMemoryDevice from './WavesMemoryDevice'
 import WavesLedgerDevice from './WavesLedgerDevice'
 import WavesLedgerDeviceMock from './WavesLedgerDeviceMock'
 
+const getConfiguredSigner = (blockchainType, state, devices, networkList, privateKeyFieldName = 'privateKey') => {
+  const account = getPersistAccount(state);
+  const targetDevice = account.decryptedWallet.entry.encrypted[0].type;
+  devices[WALLET_TYPE_METAMASK] = MetamaskPlugin;
+
+  if(devices.hasOwnProperty(targetDevice)) {
+    const networkData = getSelectedNetwork()(state);
+    const network = networkList[networkData[blockchainType]];
+    const deviceConfig = { network };
+
+    if(targetDevice === WALLET_TYPE_MEMORY) {
+      deviceConfig[privateKeyFieldName] = account.decryptedWallet.privateKey.slice(2, 66);
+    }
+
+    return targetDevice === WALLET_TYPE_METAMASK ? new MetamaskPlugin() : new devices[targetDevice](deviceConfig);
+  }
+};
+
 export const getSigner = (blockchainType, state) => {
   switch (blockchainType) {
     case BLOCKCHAIN_BITCOIN: return getBitcoinSigner(state);
@@ -64,155 +82,75 @@ export const getSigner = (blockchainType, state) => {
 };
 
 export const getBitcoinSigner = (state) => {
-  const account = getPersistAccount(state)
-  const networkData = getSelectedNetwork()(state)
-  const network = bitcoin.networks[networkData[BLOCKCHAIN_BITCOIN]]
-
-  switch (account.decryptedWallet.entry.encrypted[0].type) {
-    case WALLET_TYPE_MEMORY: {
-      const privateKey = account.decryptedWallet.privateKey.slice(2, 66)
-      return new BitcoinMemoryDevice({ privateKey, network })
-    }
-    case WALLET_TYPE_LEDGER_MOCK:
-    case WALLET_TYPE_LEDGER: {
-      return new BitcoinLedgerDeviceMock({ network })
-    }
-    case WALLET_TYPE_TREZOR_MOCK:
-    case WALLET_TYPE_TREZOR: {
-      return new BitcoinTrezorDeviceMock({ network })
-    }
-    case WALLET_TYPE_METAMASK: {
-      return new MetamaskPlugin()
-    }
-  }
-}
+  return getConfiguredSigner (BLOCKCHAIN_BITCOIN, state, {
+    [WALLET_TYPE_MEMORY]: BitcoinMemoryDevice,
+    [WALLET_TYPE_LEDGER]: BitcoinLedgerDeviceMock,
+    [WALLET_TYPE_LEDGER_MOCK]: BitcoinLedgerDeviceMock,
+    [WALLET_TYPE_TREZOR]: BitcoinTrezorDeviceMock,
+    [WALLET_TYPE_TREZOR_MOCK]: BitcoinTrezorDeviceMock
+  }, bitcoin.networks);
+};
 
 export const getBitcoinCashSigner = (state) => {
-  const account = getPersistAccount(state)
-  const networkData = getSelectedNetwork()(state)
-  const network = bitcoin.networks[networkData[BLOCKCHAIN_BITCOIN_CASH]]
-
-  switch (account.decryptedWallet.entry.encrypted[0].type) {
-    case WALLET_TYPE_MEMORY: {
-      const privateKey = account.decryptedWallet.privateKey.slice(2, 66)
-      return new BitcoinCashMemoryDevice({ privateKey, network })
-    }
-    case WALLET_TYPE_LEDGER_MOCK:
-    case WALLET_TYPE_LEDGER: {
-      return new BitcoinCashLedgerDeviceMock({ network })
-    }
-    case WALLET_TYPE_TREZOR_MOCK:
-    case WALLET_TYPE_TREZOR: {
-      return new BitcoinCashTrezorDeviceMock({ network })
-    }
-    case WALLET_TYPE_METAMASK: {
-      return new MetamaskPlugin()
-    }
-  }
-}
+  return getConfiguredSigner (BLOCKCHAIN_BITCOIN_CASH, state, {
+    [WALLET_TYPE_MEMORY]: BitcoinCashMemoryDevice,
+    [WALLET_TYPE_LEDGER]: BitcoinCashLedgerDeviceMock,
+    [WALLET_TYPE_LEDGER_MOCK]: BitcoinCashLedgerDeviceMock,
+    [WALLET_TYPE_TREZOR]: BitcoinCashTrezorDeviceMock,
+    [WALLET_TYPE_TREZOR_MOCK]: BitcoinCashTrezorDeviceMock
+  }, bitcoin.networks);
+};
 
 export const getDashSigner = (state) => {
-  const account = getPersistAccount(state);
-  const networkData = getSelectedNetwork()(state);
-  const network = bitcoin.networks[networkData[BLOCKCHAIN_DASH]];
-
-  switch (account.decryptedWallet.entry.encrypted[0].type) {
-    case WALLET_TYPE_MEMORY: {
-      const privateKey = account.decryptedWallet.privateKey.slice(2, 66);
-      return new DashMemoryDevice({ privateKey, network });
-    }
-    case WALLET_TYPE_METAMASK: {
-      return new MetamaskPlugin();
-    }
-  }
+  return getConfiguredSigner (BLOCKCHAIN_DASH, state, {
+    [WALLET_TYPE_MEMORY]: DashMemoryDevice
+  }, bitcoin.networks);
 };
 
 export const getLitecoinSigner = (state) => {
-  const account = getPersistAccount(state)
-  const networkData = getSelectedNetwork()(state)
-  const network = bitcoin.networks[networkData[BLOCKCHAIN_LITECOIN]]
-
-  switch (account.decryptedWallet.entry.encrypted[0].type) {
-    case WALLET_TYPE_MEMORY: {
-      const privateKey = account.decryptedWallet.privateKey.slice(2, 66)
-      return new BitcoinMemoryDevice({ privateKey, network })
-    }
-    case WALLET_TYPE_LEDGER_MOCK:
-    case WALLET_TYPE_LEDGER: {
-      return new BitcoinLedgerDeviceMock({ network })
-    }
-    case WALLET_TYPE_TREZOR_MOCK:
-    case WALLET_TYPE_TREZOR: {
-      return new BitcoinTrezorDeviceMock({ network })
-    }
-    case WALLET_TYPE_METAMASK: {
-      return new MetamaskPlugin()
-    }
-  }
-}
+  return getConfiguredSigner (BLOCKCHAIN_LITECOIN, state, {
+    [WALLET_TYPE_MEMORY]: BitcoinMemoryDevice,
+    [WALLET_TYPE_LEDGER]: BitcoinLedgerDeviceMock,
+    [WALLET_TYPE_LEDGER_MOCK]: BitcoinLedgerDeviceMock,
+    [WALLET_TYPE_TREZOR]: BitcoinTrezorDeviceMock,
+    [WALLET_TYPE_TREZOR_MOCK]: BitcoinTrezorDeviceMock
+  }, bitcoin.networks);
+};
 
 export const getEthereumSigner = (state) => {
-  const account = getPersistAccount(state)
+  const account = getPersistAccount(state);
 
   switch (account.selectedWallet.type) {
     case WALLET_TYPE_MEMORY: {
-      return new EthereumMemoryDevice(account.decryptedWallet.privateKey)
+      return new EthereumMemoryDevice(account.decryptedWallet.privateKey);
     }
     case WALLET_TYPE_LEDGER: {
-      return new EthereumTrezorDeviceMock()
+      return new EthereumTrezorDeviceMock();
     }
     case WALLET_TYPE_LEDGER_MOCK: {
-      return new EthereumLedgerDeviceMock()
+      return new EthereumLedgerDeviceMock();
     }
     case WALLET_TYPE_TREZOR: {
-      return new EthereumTrezorDevice()
+      return new EthereumTrezorDevice();
     }
     case WALLET_TYPE_TREZOR_MOCK: {
-      return new EthereumTrezorDeviceMock()
-    }
-  }
-}
-
-export const getNemSigner = (state) => {
-  const account = getPersistAccount(state)
-  const networkData = getSelectedNetwork()(state)
-  const network = nemSdk.model.network.data[networkData[BLOCKCHAIN_NEM] ]
-
-  switch (account.decryptedWallet.entry.encrypted[0].type) {
-    case WALLET_TYPE_MEMORY: {
-      const privateKey = account.decryptedWallet.privateKey.slice(2, 66)
-      return new NemMemoryDevice({ privateKey, network })
-    }
-    case WALLET_TYPE_TREZOR_MOCK: {
-      return new NemTrezorDeviceMock({ network })
-    }
-    case WALLET_TYPE_TREZOR: {
-      return new NemTrezorDevice({ network })
-    }
-    case WALLET_TYPE_METAMASK: {
-      return new MetamaskPlugin()
+      return new EthereumTrezorDeviceMock();
     }
   }
 };
 
-export const getWavesSigner = (state) => {
-  const account = getPersistAccount(state)
-  const networkData = getSelectedNetwork()(state)
-  const network = WavesApi[networkData[BLOCKCHAIN_WAVES]]
+export const getNemSigner = (state) => {
+  return getConfiguredSigner (BLOCKCHAIN_NEM, state, {
+    [WALLET_TYPE_MEMORY]: NemMemoryDevice,
+    [WALLET_TYPE_TREZOR]: NemTrezorDevice,
+    [WALLET_TYPE_TREZOR_MOCK]: NemTrezorDeviceMock
+  }, nemSdk.model.network.data);
+};
 
-  switch (account.decryptedWallet.entry.encrypted[0].type) {
-    case WALLET_TYPE_MEMORY: {
-      const privateKey = account.decryptedWallet.privateKey.slice(2, 66)
-      return new WavesMemoryDevice({ seedPhrase: privateKey, network })
-    }
-    case WALLET_TYPE_TREZOR_MOCK: {
-      return new WavesLedgerDeviceMock({ network })
-    }
-    case WALLET_TYPE_TREZOR: {
-      return new WavesLedgerDevice({ network })
-    }
-    case WALLET_TYPE_METAMASK: {
-      return new MetamaskPlugin()
-    }
-  }
+export const getWavesSigner = (state) => {
+  return getConfiguredSigner (BLOCKCHAIN_WAVES, state, {
+    [WALLET_TYPE_MEMORY]: WavesMemoryDevice,
+    [WALLET_TYPE_LEDGER]: WavesLedgerDevice,
+    [WALLET_TYPE_LEDGER_MOCK]: WavesLedgerDeviceMock
+  }, WavesApi, 'seedPhrase');
 };
