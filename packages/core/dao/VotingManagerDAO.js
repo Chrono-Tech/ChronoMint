c6d5eed383f05b322f55e00a12bd255a74f69487
Oleg Dizus <olegdizus@gmail.com>
2018-10-08 10:49:43 +0300

    MINT-1942 Fix wrong import

--- packages/core/dao/VotingManagerDAO.js
+++ packages/core/dao/VotingManagerDAO.js
@@ -5,7 +5,7 @@
 
 import BigNumber from 'bignumber.js'
 import { List } from 'immutable'
-import ipfs from '@chronobank/core/utils/IPFS'
+import ipfs from '../utils/IPFS'
 import PollNoticeModel from '../models/notices/PollNoticeModel'
 import PollModel from '../models/PollModel'
 import PollDetailsModel from '../models/PollDetailsModel'
