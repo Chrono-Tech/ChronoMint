62abc5399fe9722f7afce00a7722c5db5af3d639
Oleg Dizus <olegdizus@gmail.com>
2018-10-11 08:53:10 +0300

    MINT-1983 Add blockchains selectors

---
+++ packages/core/redux/blockchains/selectors.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright 2017–2018, LaborX PTY
+ * Licensed under the AGPL Version 3 license.
+ */
+
+import { DUCK_BLOCKCHAINS } from './constants'
+
+export const getBlockchains = (state) => {
+  return state.get(DUCK_BLOCKCHAINS) || {}
+}
+
+export const getBlockchainsList = (state) => {
+  return getBlockchains(state).list || []
+}
